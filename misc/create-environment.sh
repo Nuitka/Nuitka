731bf3b58daea1fe73d8762a5eca29fd4caea3c1
Kay Hayen <kayhayen@gmx.de>
2011-08-15 00:49:58 +0200

    Recognize the interactive shell as set by chroot too.

--- misc/create-environment.sh
+++ misc/create-environment.sh
@@ -30,7 +30,7 @@
 #     Please leave the whole of this copyright notice intact.
 #
 
-if [ "$0" != "bash" ] && [ "$0" != "-bash" ]
+if [ "$0" != "bash" ] && [ "$0" != "-bash" ] && [ "$0" != "/bin/bash" ]
 then
     cd `dirname $0`/..
 fi
