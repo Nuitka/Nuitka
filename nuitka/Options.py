0d0000ddd366ca6703101e9f8ee25e604816b05d
Kay Hayen <kayhayen@gmx.de>
2011-12-10 07:50:05 +0100

    Much improved optparse usage
    * Provide meaningful usage
    * Added statements about defaults for options.
    * Use groups to put related options together and get better output,
      allowing to e.g. better identify the debugging options.
    * Use default "--version" option mechanics, remove the copyright from
      the output.
    * Added "--recurse" allias to "--deep"

--- nuitka/Options.py
+++ nuitka/Options.py
@@ -36,7 +36,7 @@ Copyright (C) 2011 Kay Hayen."""
 
 from . import Utils, Tracing
 
-from optparse import OptionParser
+from optparse import OptionParser, OptionGroup
 
 import sys, os, logging
 
@@ -44,24 +44,41 @@ import sys, os, logging
 # defaults and work a bit different with parameters.
 is_nuitka_python = os.path.basename( sys.argv[0] ).lower() == "nuitka-python"
 
-parser = OptionParser()
+def getVersion():
+    return version_string.split()[1][1:]
+
+if is_nuitka_python:
+    usage = "usage: %prog [--deep] [--exe] [--execute] [options] main_module.py"
+else:
+    usage = "usage: %prog [--deep] [options] main_module.py"
+
+parser = OptionParser(
+    usage   = usage,
+    version = getVersion()
+)
 
 parser.add_option(
     "--exe",
     action  = "store_true",
     dest    = "executable",
     default = is_nuitka_python,
-    help    = "Create a standalone executable instead of a compiled extension module."
+    help    = """Create a standalone executable instead of a compiled extension module. Default is %s.""" % ( "on" if is_nuitka_python else "off" )
 )
-parser.add_option(
-    "--deep",
+
+recurse_group = OptionGroup(
+    parser,
+    "Cntrol how deep to recurse into imported modules"
+)
+
+recurse_group.add_option(
+    "--deep", "--recurse",
     action  = "store_true",
     dest    = "follow_imports",
     default = False,
     help    = "Descend into imported modules and compile them recursively."
 )
 
-parser.add_option(
+recurse_group.add_option(
     "--really-deep",
     action  = "store_true",
     dest    = "follow_stdlib",
@@ -69,63 +86,62 @@ parser.add_option(
     help    = "When --deep is used, also descend into imported modules from standard library."
 )
 
-parser.add_option(
+parser.add_option_group( recurse_group )
+
+execute_group = OptionGroup(
+    parser,
+    "Immediate execution after compilation"
+)
+
+execute_group.add_option(
     "--execute",
     action  = "store_true",
     dest    = "immediate_execution",
     default = is_nuitka_python,
-    help    = "Execute immediately the created binary. (or import the compiled module)"
+    help    = """Execute immediately the created binary (or import the compiled module). Default is %s.""" % ( "on" if is_nuitka_python else "off" )
 )
 
-parser.add_option(
+execute_group.add_option(
     "--execute-with-pythonpath",
     action  = "store_true",
     dest    = "keep_pythonpath",
     default = False,
-    help    = "When executing the created '--deep' mode binary, don't reset PYTHONPATH. When all modules are successfully included, you ought to not need PYTHONPATH anymore."
+    help    = """When immediately executing the created '--deep' mode binary, don't reset PYTHONPATH. When
+all modules are successfully included, you ought to not need PYTHONPATH anymore."""
 )
 
-parser.add_option(
-    "--trace-execution",
-    action  = "store_true",
-    dest    = "trace_execution",
-    default = False,
-    help    = "Debugging: Traced execution output."
-)
-parser.add_option(
-    "--dump-tree",
-    action  = "store_true",
-    dest    = "dump_tree",
-    default = False,
-    help    = "Debugging: Dump the final result of analysis."
+parser.add_option_group( execute_group )
+
+dump_group = OptionGroup(
+    parser,
+    "Dump options for internal tree"
 )
 
-parser.add_option(
+dump_group.add_option(
     "--dump-xml",
     action  = "store_true",
     dest    = "dump_xml",
     default = False,
-    help    = "Debugging: Dump the final result of analysis."
+    help    = """Dump the final result of optimization as XML, then exit."""
 )
 
-
-parser.add_option(
-    "--g++-only",
+dump_group.add_option(
+    "--dump-tree",
     action  = "store_true",
-    dest    = "cpp_only",
+    dest    = "dump_tree",
     default = False,
-    help    = """\
-Debugging: Only compile the would-be generated source file.
-Allows edition and translation with same options for quick
-debugging changes to the generated source."""
+    help    = """Dump the final result of optimization as text, then exit."""
 )
-parser.add_option(
+
+dump_group.add_option(
     "--display-tree",
     action  = "store_true",
     dest    = "display_tree",
     default = False,
-    help    = "Debugging: Display the final result of analysis."
+    help    = """Display the final result of optimization in a GUI, then exit."""
 )
+
+parser.add_option_group( dump_group )
 
 parser.add_option(
     "--python-version",
@@ -145,30 +161,36 @@ Use debug version or not. Default uses what you are using to run Nuitka, most
 likely a non-debug version."""
 )
 
-parser.add_option(
+codegen_group = OptionGroup(
+    parser,
+    "Code generation choices"
+)
+
+codegen_group.add_option(
     "--code-gen-no-statement-lines",
     action  ="store_false",
     dest    = "statement_lines",
     default = True,
-    help    = """\
-Statements shall have their line numbers set. Disable this for
-less precise exceptions and slightly faster code. Not recommended."""
+    help    = """Statements shall have their line numbers set. Disable this for less precise exceptions and
+slightly faster code. Not recommended. Defaults to off."""
 )
 
-parser.add_option(
+codegen_group.add_option(
     "--no-optimization",
     action  = "store_true",
     dest    = "no_optimize",
     default = False,
-    help    = "Disable all optimizations."
+    help    = """Disable all unnecessary optimizations on Python level. Defaults to off."""
 )
+
+parser.add_option_group( codegen_group )
 
 parser.add_option(
     "--output-dir",
     action  ="store",
     dest    = "output_dir",
     default = "",
-    help    = "Where intermediate and final output files should be put."
+    help    = """Specify where intermediate and final output files should be put. Defaults to current directory."""
 )
 
 parser.add_option(
@@ -176,73 +198,97 @@ parser.add_option(
     action  = "store_true",
     dest    = "windows_target",
     default = False,
-    help    = "Force compilation for windows, useful for cross-compilation."
+    help    = """Force compilation for windows, useful for cross-compilation. Defaults to off."""
 )
 
-parser.add_option(
-    "--version",
-    action  = "store_true",
-    dest    = "version",
-    default = False,
-    help    = "Only output version, then exit."
+debug_group = OptionGroup(
+    parser,
+    "Debug features"
 )
 
-parser.add_option(
+debug_group.add_option(
     "--debug",
     action  = "store_true",
     dest    = "debug",
     default = False,
-    help    = """\
-Executing all self checks possible to find errors in Nuitka, do not use for production."""
+    help    = """Executing all self checks possible to find errors in Nuitka, do not use for
+production. Defaults to off."""
 )
 
-parser.add_option(
+debug_group.add_option(
     "--unstriped",
     action  = "store_true",
     dest    = "unstriped",
     default = False,
-    help    = """\
-Keep debug info in the resulting object file for better gdb interaction."""
+    help    = """Keep debug info in the resulting object file for better gdb interaction. Defaults to off."""
 )
+
+debug_group.add_option(
+    "--trace-execution",
+    action  = "store_true",
+    dest    = "trace_execution",
+    default = False,
+    help    = """Traced execution output, output the line of code before executing it. Defaults to off."""
+)
+
+debug_group.add_option(
+    "--g++-only",
+    action  = "store_true",
+    dest    = "cpp_only",
+    default = False,
+    help    = """Compile the would-be generated source file. Allows edition and translation with same
+options for quick debugging changes to the generated source. Defaults to off."""
+)
+
+parser.add_option_group( debug_group )
 
 parser.add_option(
     "--lto",
     action  = "store_true",
     dest    = "lto",
     default = False,
-    help    = "Use link time optimizations if available and usable (g++ 4.6 and higher)."
+    help    = """Use link time optimizations if available and usable (g++ 4.6 and higher). Defaults to off."""
 )
 
-parser.add_option(
-    "--verbose",
-    action  = "store_true",
-    dest    = "verbose",
-    default = False,
-    help    = "Output details of actions take, esp. in optimizations."
+tracing_group = OptionGroup(
+    parser,
+    "Tracing features"
 )
 
-parser.add_option(
+tracing_group.add_option(
     "--show-scons",
     action  = "store_true",
     dest    = "show_scons",
     default = False,
-    help    = "Operate scons in non-quiet mode, showing the executed commands."
+    help    = """Operate Scons in non-quiet mode, showing the executed commands. Defaults to off."""
 )
 
-parser.add_option(
+tracing_group.add_option(
     "--show-progress",
     action  = "store_true",
     dest    = "show_progress",
     default = False,
-    help    = "Output progress information and statistics."
+    help    = """Provide progress information and statistics. Defaults to off."""
 )
 
-core_count = Utils.getCoreCount()
+tracing_group.add_option(
+    "--verbose",
+    action  = "store_true",
+    dest    = "verbose",
+    default = False,
+    help    = "Output details of actions take, esp. in optimizations. Can become a lot."
+)
+
+
+parser.add_option_group( tracing_group )
 
 parser.add_option(
-    "-j", "--jobs", action="store", dest = "jobs", default = core_count,
-    help = """\
-Specify the allowed number of jobs. Defaults to system CPU count (%d).""" % core_count,
+    "-j", "--jobs",
+    action  ="store",
+    dest    = "jobs",
+    default = Utils.getCoreCount(),
+    help    = """\
+Specify the allowed number of jobs. Defaults to system CPU count (%default).""",
 )
 
 if is_nuitka_python:
@@ -262,10 +308,6 @@ else:
     extra_args = []
 
 options, positional_args = parser.parse_args()
-
-if options.version:
-    Tracing.printError( version_string )
-    sys.exit(0)
 
 if options.verbose:
     logging.getLogger().setLevel( logging.DEBUG )
@@ -350,6 +392,3 @@ def isFullCompat():
 
 def isShowProgress():
     return options.show_progress and sys.stdout.isatty()
-
-def getVersion():
-    return version_string.split()[1][1:]
