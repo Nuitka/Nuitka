097b90e1ceb773196efe08601b24f2e68196bb13
ownssh <ownssh@gmail.com>
2013-03-08 08:36:40 +0100

    Python3 support main binary named "nuitka-python3" as well.

--- nuitka/Options.py
+++ nuitka/Options.py
@@ -29,7 +29,7 @@ import os, sys, logging
 
 # Indicator if we were called as "nuitka-python" in which case we assume some other
 # defaults and work a bit different with parameters.
-is_nuitka_python = Utils.basename( sys.argv[0] ).lower() == "nuitka-python"
+is_nuitka_python = Utils.basename( sys.argv[0] ).lower().startswith( "nuitka-python" )
 
 def getVersion():
     return version_string.split()[1][1:]
