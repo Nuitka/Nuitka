435b8c45fa28379abe7afc04f5fb156b9b4ae01d
Kay Hayen <kay.hayen@gmail.com>
2013-03-30 09:42:46 +0100

    Apply the lessons learned by using generic fibers on NetBSD to all variants.

--- nuitka/build/static_src/gen_ucontext_src/fibers_gen.cpp
+++ nuitka/build/static_src/gen_ucontext_src/fibers_gen.cpp
@@ -15,6 +15,7 @@
 //     See the License for the specific language governing permissions and
 //     limitations under the License.
 //
+// Implementation of process context switch for generic targets.
 
 #include "nuitka/prelude.hpp"
 
