dc6161c58fa5522827e8c6b079106c546553882b
Juan Carlos <juancarlospaco@gmail.com>
2013-11-25 04:16:45 -0300

    PEP8 and Lint Fixes

--- nuitka/freezer/Portable.py
+++ nuitka/freezer/Portable.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 #     Copyright 2013, Kay Hayen, mailto:kay.hayen@gmail.com
 #
 #     Part of "Nuitka", an optimizing Python compiler that is compatible and
@@ -15,34 +19,40 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 #
-""" Pack and copy files for portable mode.
+
+
+"""
+Pack and copy files for portable mode.
 
 This is in heavy flux now, cannot be expected to work or make sense.
 
 """
 
-import sys, subprocess, marshal
+
+import subprocess
+import sys
 from logging import debug
 
+import marshal
 from nuitka import Utils
-
 from nuitka.codegen.ConstantCodes import needsPickleInit
+
 
 python_dll_dir_name = "_python"
 
-def loadCodeObjectData( precompiled_path ):
+
+def loadCodeObjectData(precompiled_path):
     # Ignoring magic numbers, etc. which we don't have to care for much as
     # CPython already checked them (would have rejected it otherwise).
-    return open( precompiled_path, "rb" ).read()[ 8 : ]
+    return open(precompiled_path, "rb").read()[8:]
 
 
 def detectEarlyImports():
     # When we are using pickle internally (for some hard constant cases we do),
     # we need to make sure it will be available as well.
     if needsPickleInit():
-        command = "import %s;" % (
-            "pickle" if Utils.python_version >= 300 else "cPickle"
-        )
+        command = "import {};".format("pickle"
+            if Utils.python_version >= 300 else "cPickle")
     else:
         command = ""
 
@@ -50,37 +60,36 @@ def detectEarlyImports():
         command += "import inspect;"
 
     if Utils.python_version >= 300:
-        command += r'import sys; print( "\n".join( sorted( "import " + module.__name__ + " # sourcefile " + module.__file__ for module in sys.modules.values() if hasattr( module, "__file__" ) and module.__file__ != "<frozen>" ) ), file = sys.stderr )' # do not read it, pylint: disable=C0301
+        command += r'import sys; print("\n".join(sorted("import " + module.__name__ + " # sourcefile " + module.__file__ for module in sys.modules.values() if hasattr(module, "__file__") and module.__file__ != "<frozen>")), file = sys.stderr)'  # do not read it, pylint: disable=C0301  lint:ok
 
     process = subprocess.Popen(
-        args   = [ sys.executable, "-s", "-S", "-v", "-c", command ],
-        stdout = subprocess.PIPE,
-        stderr = subprocess.PIPE
+        args=[sys.executable, "-s", "-S", "-v", "-c", command],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
     )
 
     _stdout, stderr = process.communicate()
 
     result = []
 
-    debug( "Detecting early imports:" )
+    debug("Detecting early imports:")
 
     # bug of PyLint, pylint: disable=E1103
-    for line in stderr.replace( b"\r", b"" ).split( b"\n" ):
-        if line.startswith( b"import " ):
-            # print( line )
+    for line in stderr.replace(b"\r", b"").split(b"\n"):
+        if line.startswith(b"import "):
+            print(line)
 
-            parts = line.split( b" # ", 2 )
+            parts = line.split(b" # ", 2)
 
-            module_name = parts[0].split( b" ", 2 )[1]
+            module_name = parts[0].split(b" ", 2)[1]
             origin = parts[1].split()[0]
 
             if origin == b"precompiled":
                 # This is a ".pyc" file that was imported, even before we have a
                 # chance to do anything, we need to preserve it.
-                filename = parts[1][ len( b"precompiled from " ): ]
+                filename = parts[1][len(b"precompiled from "):]
 
-                debug(
-                    "Freezing module '%s' (from '%s').",
+                debug("Freezing module '%s' (from '%s').",
                     module_name,
                     filename
                 )
@@ -88,13 +97,13 @@ def detectEarlyImports():
                 result.append(
                     (
                         module_name,
-                        loadCodeObjectData( filename ),
+                        loadCodeObjectData(filename),
                         b"__init__" in filename
                     )
                 )
 
             elif origin == b"sourcefile":
-                filename = parts[1][ len( b"sourcefile " ): ]
+                filename = parts[1][len(b"sourcefile "):]
 
                 debug(
                     "Freezing module '%s' (from '%s').",
@@ -106,13 +115,12 @@ def detectEarlyImports():
                     (
                         module_name,
                         marshal.dumps(
-                            compile( open( filename ).read(), filename, "exec" )
+                            compile(open(filename).read(), filename, "exec")
                         ),
-                        Utils.basename( filename ) == b"__init__.py"
+                        Utils.basename(filename) == b"__init__.py"
                     )
                 )
 
-
-    debug( "Finished detecting early imports." )
+    debug("Finished detecting early imports.")
 
     return result
--- nuitka/freezer/PrecompiledModuleFreezer.py
+++ nuitka/freezer/PrecompiledModuleFreezer.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 #     Copyright 2013, Kay Hayen, mailto:kay.hayen@gmail.com
 #
 #     Part of "Nuitka", an optimizing Python compiler that is compatible and
@@ -15,27 +19,29 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 #
-""" Freezer for precompiled modules. Not compiled modules.
+
+
+"""
+Freezer for precompiled modules. Not compiled modules.
 
 This is including modules as bytecode and mostly intended for modules, where
 we know compiling it useless or does not make much sense, or for portable mode
 to access modules during CPython library init that cannot be avoided.
 """
 
+
 from nuitka import Utils
-
+from nuitka.codegen import BlobCodes, CodeTemplates
 from nuitka.codegen.Indentation import indented
-
-from nuitka.codegen import CodeTemplates, BlobCodes
 
 
 frozen_modules = []
 
 def addFrozenModule( frozen_module ):
-    frozen_modules.append( frozen_module )
+    frozen_modules.append(frozen_module)
 
 def getFrozenModuleCount():
-    return len( frozen_modules )
+    return len(frozen_modules)
 
 stream_data = BlobCodes.StreamData()
 
@@ -45,22 +51,18 @@ def generatePrecompiledFrozenCode():
     for frozen_module in frozen_modules:
         module_name, code_data, is_package = frozen_module
 
-        size = len( code_data )
+        size = len(code_data)
 
         # Packages are indicated with negative size.
         if is_package:
             size = -size
 
-        frozen_defs.append(
-            """(char *)"%s", (unsigned char *)%s, %d,""" % (
-                ( module_name if Utils.python_version < 300 else \
-                  module_name.decode() ),
-                stream_data.getStreamDataCode( code_data, fixed_size = True ),
-                size
-            )
-        )
+        frozen_defs.append("""(char *)"{}", (unsigned char *){}, {},""".format(
+            module_name if Utils.python_version < 300 else module_name.decode(),
+            stream_data.getStreamDataCode(code_data, fixed_size=True),
+            size))
 
     return CodeTemplates.template_frozen_modules % {
-        "stream_data"    : "".join( stream_data.encodeStreamData() ),
-        "frozen_modules" : indented( frozen_defs )
+        "stream_data": "".join(stream_data.encodeStreamData()),
+        "frozen_modules": indented(frozen_defs)
     }
