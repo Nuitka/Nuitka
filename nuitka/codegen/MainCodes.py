e8e6041d73a28f736840460fe49b971106122202
Kay Hayen <kay.hayen@gmail.com>
2014-04-08 08:07:31 +0200

    Fix, the condition for presence of hash_randomization was broken.

--- nuitka/codegen/MainCodes.py
+++ nuitka/codegen/MainCodes.py
@@ -82,7 +82,7 @@ def getMainCode(codes, context):
             if hasattr( sys.flags, "unicode" ) else 0 ),
         "python_sysflag_bytes_warning" : sys.flags.bytes_warning,
         "python_sysflag_hash_randomization" : ( sys.flags.hash_randomization
-            if hasattr( sys.flags, "hash_randomization" )  and "no_randomization" not in python_flags else 0 ),
+            if (hasattr( sys.flags, "hash_randomization" ) and "no_randomization" not in python_flags) else 0 ),
         "code_identifier"      : code_identifier.getCodeTemporaryRef()
     }
 
