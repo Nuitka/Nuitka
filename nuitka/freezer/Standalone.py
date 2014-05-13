6680746584f49e4d3e619952fba66186e635a390
Kay Hayen <kay.hayen@gmail.com>
2014-05-13 09:28:32 +0200

    Standalone: Added "idna" encoding.

--- nuitka/freezer/Standalone.py
+++ nuitka/freezer/Standalone.py
@@ -386,7 +386,7 @@ def detectEarlyImports():
                       '        pass\n'
     else:
         # TODO: Should recursively include all of encodings module.
-        import_code = "import encodings.utf_8;import encodings.ascii;"
+        import_code = "import encodings.utf_8;import encodings.ascii;import encodings.idna"
 
         if Utils.getOS() == "Windows":
             import_code += "import encodings.mbcs;import encodings.cp437;"
