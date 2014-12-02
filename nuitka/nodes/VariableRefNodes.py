15538fc42e89486d8e410dd2af00d34f7600f98d
Kay Hayen <kay.hayen@gmail.com>
2014-12-02 20:19:22 +0100

    Cleanup, pair the nodes together that are supposed to do the same.
    * This makes it easier to analyse their differences.
    
    * Ultimately we may or may not merge these node types. But it may be
      best to keep them distinct, to save analysis time.

--- nuitka/nodes/VariableRefNodes.py
+++ nuitka/nodes/VariableRefNodes.py
@@ -178,61 +178,6 @@ Replaced read-only module attribute '__package__' with constant value."""
         return not self.variable_trace.mustHaveValue()
 
 
-class ExpressionTargetVariableRef(ExpressionVariableRef):
-    kind = "EXPRESSION_TARGET_VARIABLE_REF"
-
-    # TODO: Remove default and correct argument order later.
-    def __init__(self, variable_name, source_ref, variable = None):
-        ExpressionVariableRef.__init__(self, variable_name, source_ref)
-
-        self.variable_version = None
-
-        # TODO: Remove setVariable, once not needed anymore and in-line to
-        # here.
-        if variable is not None:
-            self.setVariable(variable)
-            assert variable.getName() == variable_name
-
-    def getDetails(self):
-        if self.variable is None:
-            return {
-                "name" : self.variable_name
-            }
-        else:
-            return {
-                "name"     : self.variable_name,
-                "variable" : self.variable,
-                "version"  : self.variable_version
-            }
-
-    def makeCloneAt(self, source_ref):
-        result = self.__class__(
-            variable_name = self.variable_name,
-            source_ref    = source_ref,
-            variable      = self.variable
-        )
-
-        return result
-
-    def computeExpression(self, constraint_collection):
-        assert False
-
-    @staticmethod
-    def isTargetVariableRef():
-        return True
-
-    def getVariableVersion(self):
-        assert self.variable_version is not None, self
-
-        return self.variable_version
-
-    def setVariable(self, variable):
-        ExpressionVariableRef.setVariable(self, variable)
-
-        self.variable_version = variable.allocateTargetNumber()
-        assert self.variable_version is not None
-
-
 class ExpressionTempVariableRef(NodeBase, ExpressionMixin):
     kind = "EXPRESSION_TEMP_VARIABLE_REF"
 
@@ -296,6 +241,61 @@ class ExpressionTempVariableRef(NodeBase, ExpressionMixin):
     # Python3 only, it updates temporary variables that are closure variables.
     def setVariable(self, variable):
         self.variable = variable
+
+
+class ExpressionTargetVariableRef(ExpressionVariableRef):
+    kind = "EXPRESSION_TARGET_VARIABLE_REF"
+
+    # TODO: Remove default and correct argument order later.
+    def __init__(self, variable_name, source_ref, variable = None):
+        ExpressionVariableRef.__init__(self, variable_name, source_ref)
+
+        self.variable_version = None
+
+        # TODO: Remove setVariable, once not needed anymore and in-line to
+        # here.
+        if variable is not None:
+            self.setVariable(variable)
+            assert variable.getName() == variable_name
+
+    def getDetails(self):
+        if self.variable is None:
+            return {
+                "name" : self.variable_name
+            }
+        else:
+            return {
+                "name"     : self.variable_name,
+                "variable" : self.variable,
+                "version"  : self.variable_version
+            }
+
+    def makeCloneAt(self, source_ref):
+        result = self.__class__(
+            variable_name = self.variable_name,
+            source_ref    = source_ref,
+            variable      = self.variable
+        )
+
+        return result
+
+    def computeExpression(self, constraint_collection):
+        assert False
+
+    @staticmethod
+    def isTargetVariableRef():
+        return True
+
+    def getVariableVersion(self):
+        assert self.variable_version is not None, self
+
+        return self.variable_version
+
+    def setVariable(self, variable):
+        ExpressionVariableRef.setVariable(self, variable)
+
+        self.variable_version = variable.allocateTargetNumber()
+        assert self.variable_version is not None
 
 
 class ExpressionTargetTempVariableRef(ExpressionTempVariableRef):
