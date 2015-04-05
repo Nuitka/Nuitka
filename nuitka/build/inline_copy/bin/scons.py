f10fee2c7c38c4e494a9e01c72d6b419a24b69be
Kay Hayen <kay.hayen@gmail.com>
2015-04-05 22:11:12 +0200

    Python3: Check that inline copy of scons is not run with Python3.
    * This happens to MacOS users seemingly.

--- nuitka/build/inline_copy/bin/scons.py
+++ nuitka/build/inline_copy/bin/scons.py
@@ -13,6 +13,12 @@ if __name__ == "__main__":
         )
     )
 
+    if sys.version_info[0] >= 3:
+        sys.exit("""\
+Error, scons must be run with Python3. Our attempts at guessing to find a
+Python2 version must have failed. Ideally you would install Scons, then
+Nuitka would use it and not face the issue.""")
+
     import SCons.Script
 
     # this does all the work, and calls sys.exit
