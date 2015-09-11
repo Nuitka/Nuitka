c7606412e9cb7ba746f012772ee0cdc535949c16
Kay Hayen <kay.hayen@gmail.com>
2015-09-11 10:23:51 +0200

    MacOS: Added support for Anaconda accelerated mode, by setting path to Python DLL.
    * Standalone mode already works supposedly, but this makes accelerated
      binaries without the need to copy the DLL alongside the binary.
    
    * Need to check if similar can be done for Windows too or needs to be
      done for a potential Linux variant of Anaconda Python.

--- nuitka/MainControl.py
+++ nuitka/MainControl.py
@@ -479,9 +479,8 @@ def runScons(main_module, quiet):
 
     if not Options.isStandaloneMode() and \
        not Options.shallMakeModule() and \
-       Utils.getOS() == "Windows" and \
        isUninstalledPython():
-        options["win_copy_dll"] = "true"
+        options["uninstalled_python"] = "true"
 
     if getFrozenModuleCount():
         options["frozen_modules"] = str(
