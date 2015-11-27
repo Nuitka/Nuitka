23e006627ae2ca927c00b9d56321e664bbc887b5
Kay Hayen <kay.hayen@gmail.com>
2015-11-27 10:22:41 +0100

    Windows: Properly handle unicode command line arguments in Python3.
    * Using dedicated Windows API to parse command line, the general
      approach seems to fail.
    
    * Moved FreeBSD stuff to the outside.
    
    * Also split conversion from usage of command line arguments for
      cleaner code that does things only once.

--- nuitka/build/include/nuitka/helpers.hpp
+++ nuitka/build/include/nuitka/helpers.hpp
@@ -631,9 +631,15 @@ extern void enhancePythonTypes( void );
 // Setup meta path based loader if any.
 extern void setupMetaPathBasedLoader( void );
 
-// Parse the command line parameters and provide it to sys module. It may
-// manipulate them, therefore argc is passed as a pointer.
-extern bool setCommandLineParameters( int *argc, char *argv[], bool initial );
+// Parse the command line parameters and provide it to "sys" built-in module.
+
+#if PYTHON_VERSION >= 300
+typedef wchar_t ** argv_type_t;
+extern argv_type_t convertCommandLineParameters( int argc, char **argv );
+#else
+typedef char ** argv_type_t;
+#endif
+extern bool setCommandLineParameters( int argc, argv_type_t argv, bool initial );
 
 // Replace built-in functions with ones that accept compiled types too.
 extern void patchBuiltinModule( void );
