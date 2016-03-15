a244a8cb579839fdf56510c528bec9887d22417f
Kay Hayen <kay.hayen@gmail.com>
2016-03-15 08:06:49 +0100

    Standalone: More hidden dependencies
    * Also cleaned up where "locateDLL" lives, moved to a dedicated module.

--- nuitka/plugins/standard/ImplicitImports.py
+++ nuitka/plugins/standard/ImplicitImports.py
@@ -23,15 +23,17 @@ to add to this and submit patches to make it more complete.
 """
 
 import shutil
-from sys import getfilesystemencoding
 
 from nuitka.plugins.PluginBase import NuitkaPluginBase
 from nuitka.PythonVersions import python_version
+from nuitka.utils.SharedLibraries import locateDLL
 from nuitka.utils.Utils import basename, getOS, joinpath
 
 
 class NuitkaPluginPopularImplicitImports(NuitkaPluginBase):
     def getImplicitImports(self, full_name):
+        # Many variables, branches, due to the many cases, pylint: disable=R0912,R0915
+        # TODO: Move this out to some kind of configuration format.
         elements = full_name.split('.')
 
         if elements[0] in ("PyQt4", "PyQt5"):
@@ -42,11 +44,59 @@ class NuitkaPluginPopularImplicitImports(NuitkaPluginBase):
 
             child = elements[1] if len(elements) > 1 else None
 
-            if child == "QtGui":
+            if child in ("QtGui", "QtAssistant", "QtDBus", "QtDeclarative",
+                         "QtDesigner", "QtHelp", "QtNetwork", "QtScript",
+                         "QtScriptTools", "QtSvg", "QtTest", "QtWebKit",
+                         "QtXml", "QtXmlPatterns", "QtPrintSupport",
+                         "QtWebKitWidgets"):
                 yield elements[0] + ".QtCore"
 
-            if child == "QtWidgets":
+            if child in ("QtDeclarative", "QtWebKit", "QtXmlPatterns",
+                         "QtPrintSupport", "QtWebKitWidgets"):
+                yield elements[0] + ".QtNetwork"
+
+            if child == "QtScriptTools":
+                yield elements[0] + ".QtScript"
+
+            if child in ("QtWidgets", "QtDeclarative", "QtDesigner", "QtHelp",
+                         "QtScriptTools", "QtSvg", "QtTest", "QtWebKit",
+                         "QtPrintSupport", "QtWebKitWidgets"):
                 yield elements[0] + ".QtGui"
+
+            if full_name in ("PyQt5.QtDesigner", "PyQt5.QtHelp", "PyQt5.QtTest",
+                             "PyQt5.QtPrintSupport", "PyQt5.QtSvg",
+                             "PyQt5.QtWebKitWidgets"):
+                yield "PyQt5.QtWidgets"
+
+            if full_name in ("PyQt5.QtPrintSupport",):
+                yield "PyQt5.QtSvg"
+
+            if full_name in ("PyQt5.QtWebKitWidgets",):
+                yield "PyQt5.QtWebKit"
+                yield "PyQt5.QtPrintSupport"
+
+        elif full_name == "PySide.QtDeclarative":
+            yield "PySide.QtGui"
+        elif full_name == "PySide.QtHelp":
+            yield "PySide.QtGui"
+        elif full_name == "PySide.QtOpenGL":
+            yield "PySide.QtGui"
+        elif full_name == "PySide.QtScriptTools":
+            yield "PySide.QtScript"
+            yield "PySide.QtGui"
+        elif full_name == "PySide.QtSql":
+            yield "PySide.QtGui"
+        elif full_name == "PySide.QtSvg":
+            yield "PySide.QtGui"
+        elif full_name == "PySide.QtTest":
+            yield "PySide.QtGui"
+        elif full_name == "PySide.QtUiTools":
+            yield "PySide.QtGui"
+            yield "PySide.QtXml"
+        elif full_name == "PySide.QtWebKit":
+            yield "PySide.QtGui"
+        elif full_name == "PySide.phonon":
+            yield "PySide.QtGui"
         elif full_name == "lxml.etree":
             yield "gzip"
             yield "lxml._elementpath"
@@ -62,10 +112,22 @@ class NuitkaPluginPopularImplicitImports(NuitkaPluginBase):
             yield "_ctypes"
         elif full_name == "gi._gi":
             yield "gi._error"
+        elif full_name == "gi._gi_cairo":
+            yield "cairo"
+        elif full_name == "cairo._cairo":
+            yield "gi._gobject"
         elif full_name in ("Tkinter", "tkinter"):
             yield "_tkinter"
-        elif full_name in ("cryptography.hazmat.bindings._openssl",):
+        elif full_name in ("cryptography.hazmat.bindings._openssl",
+                           "cryptography.hazmat.bindings._constant_time",
+                           "cryptography.hazmat.bindings._padding"):
             yield "_cffi_backend"
+        elif full_name == "_dbus_glib_bindings":
+            yield "_dbus_bindings"
+        elif full_name == "_mysql":
+            yield "_mysql_exceptions"
+        elif full_name == "lxml.objectify":
+            yield "lxml.etree"
 
     module_aliases = {
         "requests.packages.urllib3" : "urllib3",
@@ -95,41 +157,11 @@ class NuitkaPluginPopularImplicitImports(NuitkaPluginBase):
 
         return False
 
-    def locateDLL(self, dll_name):
-        import ctypes.util
-
-        dll_name = ctypes.util.find_library(dll_name)
-
-        import subprocess
-        process = subprocess.Popen(
-            args   = ["/sbin/ldconfig", "-p"],
-            stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE,
-        )
-        stdout, _stderr = process.communicate()
-
-        dll_map = {}
-
-        for line in stdout.splitlines()[1:]:
-            assert line.count(b"=>") == 1, line
-            left, right = line.strip().split(b" => ")
-            assert b" (" in left, line
-            left = left[:left.rfind(b" (")]
-
-            if python_version >= 300:
-                left = left.decode(getfilesystemencoding())
-                right = right.decode(getfilesystemencoding())
-
-            if left not in dll_map:
-                dll_map[left] = right
-
-        return dll_map[dll_name]
-
     def considerExtraDlls(self, dist_dir, module):
         full_name = module.getFullName()
 
         if getOS() == "Linux" and full_name == "uuid":
-            uuid_dll_path = self.locateDLL("uuid")
+            uuid_dll_path = locateDLL("uuid")
             dist_dll_path = joinpath(dist_dir, basename(uuid_dll_path))
 
             shutil.copy(uuid_dll_path, dist_dir)
