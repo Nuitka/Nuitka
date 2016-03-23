22b65a8cf7028028da0ce4ab29588c81cc56c34f
Kay Hayen <kay.hayen@gmail.com>
2016-03-23 09:53:19 +0100

    Tests: Minor cleanups

--- tests/programs/deep/some_package/DeepChild.py
+++ tests/programs/deep/some_package/DeepChild.py
@@ -15,10 +15,11 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 #
+from __future__ import print_function
 
 print( "Importing child." )
 
 class A:
-   pass
+    pass
 
 print("Class defined here, has these vars", vars(A))
