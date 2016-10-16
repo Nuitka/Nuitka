b14ffcee744a38cf748b5afc5a95e53d2cfc75cf
Kay Hayen <kay.hayen@gmail.com>
2016-10-16 18:58:45 +0200

    Release: More robust way to tell current git branch.

--- misc/make-upload.py
+++ misc/make-upload.py
@@ -45,7 +45,9 @@ def checkAtHome():
 checkAtHome()
 
 nuitka_version = subprocess.check_output("./bin/nuitka --version", shell = True).strip()
-branch_name = subprocess.check_output("git name-rev --name-only HEAD".split()).strip()
+branch_name = subprocess.check_output(
+    "git symbolic-ref --short HEAD".split()
+).strip()
 
 assert branch_name in (b"master", b"develop", b"release/" + nuitka_version, b"hotfix/" +nuitka_version), branch_name
 
