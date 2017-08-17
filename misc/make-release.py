7d13b419775f926bd50b9841ed64941d3ea04451
Kay Hayen <kay.hayen@gmail.com>
2017-08-17 10:14:58 +0200

    Release: We no longer use egg code in our release.

--- misc/make-release.py
+++ misc/make-release.py
@@ -110,8 +110,7 @@ for filename in os.listdir('.'):
         assert 0 == os.system(
             "tar --wildcards --delete --file " + new_name[:-3] + \
             " Nuitka*/*.pdf" + \
-            " Nuitka*/build/inline_copy" + \
-            " Nuitka*/Nuitka.egg-info"
+            " Nuitka*/build/inline_copy"
         )
         assert 0 == os.system("gzip -9 -n " + new_name[:-3])
 
