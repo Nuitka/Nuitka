7ce34992d6d9ec24d7875ecab5af950f285fbc63
Andrew Leech <andrew@alelec.net>
2018-02-20 17:46:10 +1100

    Remove obsolete nuitka binary search

--- nuitka/distutils/bdist_nuitka.py
+++ nuitka/distutils/bdist_nuitka.py
@@ -97,10 +97,6 @@ class build(distutils.command.build.build):
 
         output_dir = build_lib
 
-        nuitka_binary = getExecutablePath("nuitka")
-        if nuitka_binary is None:
-            sys.exit("Error, cannot find nuitka binary in PATH.")
-
         command = [
             sys.executable,
             "-m", "nuitka",
