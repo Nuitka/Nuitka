e133348f9fd52b0a75e0d34d8c70a1a6d7563c7b
Kay Hayen <kay.hayen@gmail.com>
2018-08-23 23:04:10 +0200

    PyPI: Added tag to indicate 3.7 as supported.

--- setup.py
+++ setup.py
@@ -253,6 +253,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
 
         # We depend on CPython.
         "Programming Language :: Python :: Implementation :: CPython",
