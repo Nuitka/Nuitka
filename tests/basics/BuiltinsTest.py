78722853e4a4a46bca266cb4c0ba921b63d2181b
bksahu <bablusahoo16@gmail.com>
2019-03-10 16:02:54 +0530

    Update BuiltinAnyNodes

--- tests/basics/BuiltinsTest.py
+++ tests/basics/BuiltinsTest.py
@@ -530,7 +530,7 @@ print(any([None, None, None]))
 print(any([None, 4, None]))
 print(any([]))
 print(any(range(20)))
-#print(any([0] * 20000))
+print(any([0] * 20000))
 
 S = [40, 60, 30]
 print(any(x > 42 for x in S))
@@ -544,11 +544,6 @@ try:
     print(any(1.0))
 except Exception as e:
     print("Object not iterable")
-
-try:
-    print(any(1,2))
-except Exception as e:
-    print("More than one argument given")
 
 try:
     print(any(float))
