35cebda157acfc112b29557fc4b428d3a181a0b8
Riya <riyad17031998@gmail.com>
2019-03-24 01:59:12 +0530

    Update BINARY_ADD.py

--- nuitka/tools/gen_helper_code/BINARY_ADD.py
+++ nuitka/tools/gen_helper_code/BINARY_ADD.py
@@ -1,43 +1,21 @@
 from jinja2 import Template
 
-def isIntTypeExact(a):
+def add(a, b):
 	if(isinstance(a,str)):
-		return 0
+		if(isinstance(b,str)):
+			c = a+b
+			return c
+		print("Not an integer")
 	else:
 		if (type(int(a)))==int:
 			if(isinstance(a,float)):
 				print("Not an Integer")
 			else:
-				return 1
+				c = a+b
+				return c
 
-def add(a, b):
-	return a+b
-
-def concat(a, b):
-	return a+b
-
-
-checkIntopa = Template("{{ isIntTypeExact(a=3) }}")
-resa = checkIntopa.render(isIntTypeExact=isIntTypeExact)
-
-checkIntopb = Template("{{isIntTypeExact(a=5) }}")
-resb = checkIntopb.render(isIntTypeExact=isIntTypeExact)
-
-result = ""
-
-if(resa == '1'):
-	if(resb == '1'):
-		addtemp = Template("{{ add(a=2, b=5) }}")
-		result = addtemp.render(add=add)	
-elif(resa == '0'):
-	if(resb == '0'):
-		concat_temp = Template("{{ concat(a='2', b='5') }}")
-		result = concat_temp.render(concat=concat)
-
-print(result)
-
-"""
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
 addtemp = Template("Add a + b = {{ add(a=2, b=4) }}")
-add = addtemp.render(add=add)
-print(add)
-"""
+addition = addtemp.render(add=add)
+print(addition)
