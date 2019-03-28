5a6b768a260330cd39462f989eca66b184754891
Tommy Li <tommyli3318@gmail.com>
2019-03-27 21:28:23 -0700

    created Urllib3Using.py

---
+++ tests/standalone/Urllib3Using.py
@@ -0,0 +1,22 @@
+import urllib3
+
+# testing request
+http = urllib3.PoolManager()
+r = http.request('GET', 'http://httpbin.org/robots.txt')
+r = http.request('POST','http://httpbin.org/post',fields={'hello': 'world'})
+
+# testing response
+r.status
+r.data
+r.headers
+
+# testing JSON content
+import json
+r = http.request('GET', 'http://httpbin.org/ip')
+json.loads(r.data.decode('utf-8'))
+
+# testing binary content
+r = http.request('GET', 'http://httpbin.org/bytes/8')
+r.data
+
+print('Reached end of file')
