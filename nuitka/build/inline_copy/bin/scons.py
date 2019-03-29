85b70b94992a7a21d1551d0918d5e02561d61418
Kay Hayen <kay.hayen@gmail.com>
2019-03-29 08:04:39 +0100

    Scons: Use newer scons on Python 2.7 as well.

--- nuitka/build/inline_copy/bin/scons.py
+++ nuitka/build/inline_copy/bin/scons.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                 "..",
                 "lib",
                 "scons-2.3.2"
-                  if sys.version_info < (3,0) else
+                  if sys.version_info < (2,7) else
                 "scons-3.0.4"
             )
         )
