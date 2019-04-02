5d70209d20d09d5aabac6956e1d643ed2920d22e
Pranamika <pranamika.pihu1998@gmail.com>
2019-04-02 18:48:07 +0530

    This commit contributes to solving Nuitka#229
    This is a test to check if PyPI package Six is supported by Nuitka

--- tests/standalone/six.py
+++ tests/standalone/six.py
@@ -4,24 +4,22 @@ import sys
 
 MAXSIZE = sys.maxsize
 
-def test_dispatch_types(value):
+def dispatch_types(value):
     assert isinstance(six.MAXSIZE + 23, six.integer_types)
     assert isinstance(six.u("hi"), six.string_types)
-    
     class X(object):
         pass
-    
+
     assert isinstance(X, six.class_types)
 
-    
 
-def test__import_module(name):
+def import_module(name):
     from logging import handlers
     m = six._import_module("logging.handlers")
     assert m is handlers
 
 
-def test__add_doc(func, doc):
+def add_doc(func, doc):
     def f():
         """Icky doc"""
         pass
