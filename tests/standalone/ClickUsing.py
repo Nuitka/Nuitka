597610a384615281c2041b7de31fcf071bc679be
tab1tha <tambetachere@gmail.com>
2019-04-02 22:16:39 +0100

    [TEST]Nuitka standalone test for Click PyPI package
    Click is a python package that supports arbitrary nesting
    of commands, automatic help generation and lazy loading
    of subcommands at runtime.
    
    The Click package is tested by passing its options as
    command line arguments while invoking the program.
    
    This commit contributes to https://github.com/Nuitka/Nuitka/issues/229
    
    Signed-off-by: tab1tha <tambetachere@gmail.com>

---
+++ tests/standalone/ClickUsing.py
@@ -0,0 +1,22 @@
+import click
+
+# nuitka-skip-unless-imports: click
+
+@click.command()
+@click.option("--count", default=3, help="Number of greetings.")
+@click.option("--fname", prompt="Your name", help="The person to greet.")
+@click.option("--sname", prompt="Your name", help="The person to greet.")
+
+def hello(count, fname, sname):
+        """Simple program that greets NAME for a total of COUNT times."""
+        for _ in range(count):
+                click.echo("Hello, %s %s!" % (fname, sname))
+
+def main(*args):
+        hello(*args)
+
+
+if __name__ == '__main__':
+        commands = '--count=7 --fname=Tabi --sname=Derick'
+        main(commands.split())
+
