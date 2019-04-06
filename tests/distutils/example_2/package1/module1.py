88b49b599c6ad0d5dadc59dd57941916d68b17d9
Mario Costa <mario.silva.costa@gmail.com>
2019-04-06 05:51:09 +0100

    added test for bdist_nuitka, to pass custom options.

---
+++ tests/distutils/example_2/package1/module1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+def module1_f1( var1 ):
+    print_str = "" + __name__ + " : function module1_f1: " +  var1 + " "
+    print(print_str, end='')
+    print()
