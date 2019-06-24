c492fcdb584c7cce891f12f4728eef76f58b483d
Jorj X. McKie <jorj.x.mckie@outlook.de>
2019-06-24 04:44:23 -0400

    Update NumpyPlugin.py

--- nuitka/plugins/standard/NumpyPlugin.py
+++ nuitka/plugins/standard/NumpyPlugin.py
@@ -57,6 +57,8 @@ def remove_suffix(string, suffix):
 
 
 def get_sys_prefix():
+    """ Return sys.prefix as guarantied abspath format.
+    """
     sys_prefix = getattr(sys, "real_prefix", getattr(sys, "base_prefix", sys.prefix))
     sys_prefix = os.path.abspath(sys_prefix)
     return sys_prefix
