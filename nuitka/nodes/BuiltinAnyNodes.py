35ac1d011bf2ebe836f1f7c9dae68e2672c477f5
Kay Hayen <kay.hayen@gmail.com>
2019-06-28 13:08:55 +0200

    Minor cleanups

--- nuitka/nodes/BuiltinAnyNodes.py
+++ nuitka/nodes/BuiltinAnyNodes.py
@@ -1,5 +1,4 @@
-#     Copyright 2019, Kay Hayen, mailto:kay.hayen@gmail.com
-#                     Batakrishna Sahu, mailto:batakrishna.sahu@suiit.ac.in
+#     Copyright 2019, Batakrishna Sahu, mailto:<Batakrishna.Sahu@suiit.ac.in>
 #
 #     Part of "Nuitka", an optimizing Python compiler that is compatible and
 #     integrates with CPython, but also works on its own.
