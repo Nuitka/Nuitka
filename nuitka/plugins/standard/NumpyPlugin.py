73b10ec5f610e72e24751a74418106edd4bee1d5
Kay Hayen <kay.hayen@gmail.com>
2019-07-13 17:27:38 +0200

    Minor cleanups

--- nuitka/plugins/standard/NumpyPlugin.py
+++ nuitka/plugins/standard/NumpyPlugin.py
@@ -235,7 +235,7 @@ class NumpyPlugin(NuitkaPluginBase):
         Returns:
             empty tuple
         """
-        # pylint: disable=too-many-branches,too-many-return-statements,too-many-locals,too-many-statements
+        # pylint: disable=too-many-branches,too-many-locals,too-many-return-statements,too-many-statements
         full_name = module.getFullName()
         if full_name not in ("numpy", "scipy", "matplotlib", "sklearn.datasets"):
             return ()
