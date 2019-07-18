fd562900e39131eafa8764394809f5bdb7ecebd9
Tommy Li <tommyli3318@gmail.com>
2019-07-18 15:06:11 -0700

    extend to more packages, added main()

--- tests/PyPI-pytest/run_all.py
+++ tests/PyPI-pytest/run_all.py
@@ -61,15 +61,27 @@ class bcolors:
 # TODO: Get closer to 50 items :)
 
 packages = {
-    "certifi": {
-        "url": "https://github.com/certifi/python-certifi.git",
+    "chardet": {
+        "url": "https://github.com/chardet/chardet.git",
         "requirements_file": None,
+        "ignored_tests": None,
+    },
+
+    "colorama": {
+        "url": "https://github.com/tartley/colorama.git",
+        "requirements_file": "requirements-dev.txt",
         "ignored_tests": None,
     },
 
     "dateutil": {
         "url": "https://github.com/dateutil/dateutil.git",
         "requirements_file": "requirements-dev.txt",
+        "ignored_tests": None,
+    },
+
+    "idna": {
+        "url": "https://github.com/kjd/idna.git",
+        "requirements_file": None,
         "ignored_tests": None,
     },
 
@@ -97,6 +109,12 @@ packages = {
         "ignored_tests": None,
     },
 
+    "rsa": {
+        "url": "https://github.com/sybrenstuvel/python-rsa.git",
+        "requirements_file": None,
+        "ignored_tests": None,
+    },
+
     "urllib3": {
         "url": "https://github.com/urllib3/urllib3.git",
         "requirements_file": "dev-requirements.txt",
@@ -104,203 +122,212 @@ packages = {
             "test/test_no_ssl.py",
         )
     },
-
 }
 
-base_dir = os.getcwd()
 
-search_mode = createSearchMode()
 
-results = []
+def main():
+    base_dir = os.getcwd()
 
-for package_name, details in sorted(packages.items()):
-    active = search_mode.consider(dirname=None, filename=package_name)
+    search_mode = createSearchMode()
 
-    if not active:
-        continue
+    results = []
 
-    # pyyaml: invalid command 'bdist_nuitka'
-    if package_name in ("pyyaml"):
-        continue
+    for package_name, details in sorted(packages.items()):
+        active = search_mode.consider(dirname=None, filename=package_name)
 
-    try:
-        with withVirtualenv("venv_%s" % package_name) as venv:
-            dist_dir = os.path.join(venv.getVirtualenvDir(), package_name, "dist")
+        if not active:
+            continue
 
-            # setup the virtualenv for testing
-            venv.runCommand("git clone %s %s" % (details["url"], package_name))
+        if package_name in (
+            "pyyaml", # invalid command 'bdist_nuitka'
+        ):
+            if search_mode.abortIfExecuted():
+                break
+            continue
 
-            # delete ignored tests if any
-            if details["ignored_tests"]:
-                for test in details["ignored_tests"]:
-                    venv.runCommand("rm %s" % os.path.join(package_name, test))
+        try:
+            with withVirtualenv("venv_%s" % package_name) as venv:
+                dist_dir = os.path.join(venv.getVirtualenvDir(), package_name, "dist")
 
-            cmds = [
-                "python -m pip install pytest",
-                "cd %s" % os.path.join(os.path.dirname(nuitka.__file__), ".."),
-                "python setup.py develop",
-                "cd %s" % os.path.join(venv.getVirtualenvDir(), package_name),
-            ]
+                # setup the virtualenv for testing
+                venv.runCommand("git clone %s %s" % (details["url"], package_name))
 
-            if details["requirements_file"]:
+                # delete ignored tests if any
+                if details["ignored_tests"]:
+                    for test in details["ignored_tests"]:
+                        venv.runCommand("rm %s" % os.path.join(package_name, test))
+
+                cmds = [
+                    "python -m pip install pytest",
+                    "cd %s" % os.path.join(os.path.dirname(nuitka.__file__), ".."),
+                    "python setup.py develop",
+                    "cd %s" % os.path.join(venv.getVirtualenvDir(), package_name),
+                ]
+
+                if details["requirements_file"]:
+                    cmds += [
+                        "python -m pip install -r %s" % details["requirements_file"],
+                    ]
+
+                # build uncompiled .whl
                 cmds += [
-                    "python -m pip install -r %s" % details["requirements_file"],
+                    "python setup.py bdist_wheel",
                 ]
 
-            # build uncompiled .whl
-            cmds += [
-                "python setup.py bdist_wheel",
-            ]
+                venv.runCommand(
+                    commands=cmds
+                )
 
-            venv.runCommand(
-                commands=cmds
+                # install and print out if the active .whl is compiled or not
+                venv.runCommand(
+                    commands=[
+                        "python -m pip install -U %s" % os.path.join(dist_dir, os.listdir(dist_dir)[0]),
+                        "python -c print(getattr(__import__('%s'),'__compiled__','__uncompiled_version__'))" % package_name,
+                    ]
+                )
+
+                # get uncompiled pytest results
+                uncompiled_stdout, uncompiled_stderr = venv.runCommandWithOutput(
+                    commands=[
+                        "cd %s" % package_name,
+                        "python -m pytest --disable-warnings",
+                    ]
+                )
+
+                # build nuitka compiled .whl
+                venv.runCommand(
+                    commands=[
+                        "cd %s" % package_name,
+                        "rm -rf dist",
+                        "python setup.py bdist_nuitka",
+                    ]
+                )
+
+                # install and print out if the active .whl is compiled or not
+                venv.runCommand(
+                    commands=[
+                        "python -m pip install -U %s" % os.path.join(dist_dir, os.listdir(dist_dir)[0]),
+                        "python -c print(getattr(__import__('%s'),'__compiled__','__uncompiled_version__'))" % package_name,
+                    ]
+                )
+
+                # get compiled pytest results
+                compiled_stdout, compiled_stderr = venv.runCommandWithOutput(
+                    commands=[
+                        "cd %s" % package_name,
+                        "python -m pytest --disable-warnings",
+                    ]
+                )
+
+        except Exception as e:
+            my_print("Package", package_name, "ran into an exception during execution, traceback: ")
+            my_print(e)
+            if search_mode.abortIfExecuted():
+                break
+            continue
+
+
+
+        os.chdir(base_dir)
+
+        # TODO: currently not working due to permission errors on removing .git files
+        removeDirectory(os.path.join(base_dir, "venv_%s" % package_name), ignore_errors=False)
+
+        # compare outputs
+        stdout_diff = compareOutput(
+            "stdout",
+            uncompiled_stdout,
+            compiled_stdout,
+            ignore_warnings=True,
+            ignore_infos=True,
+            syntax_errors=True,
+        )
+
+        stderr_diff = compareOutput(
+            "stderr",
+            uncompiled_stderr,
+            compiled_stderr,
+            ignore_warnings=True,
+            ignore_infos=True,
+            syntax_errors=True,
+        )
+
+
+        results.append((package_name,stdout_diff,stderr_diff))
+
+        exit_code = stdout_diff or stderr_diff
+
+
+        my_print(
+            bcolors.RED if exit_code else bcolors.GREEN,
+            "\n=================================================================================",
+            bcolors.ENDC
+        )
+
+        my_print(
+            bcolors.RED if exit_code else bcolors.GREEN,
+            "--- %s ---" % package_name,
+            "exit_stdout:",
+            stdout_diff,
+            "exit_stderr:",
+            stderr_diff,
+            bcolors.ENDC
+        )
+
+        if exit_code:
+            my_print(
+                bcolors.RED,
+                "Error, outputs differed for package %s." % package_name
+            )
+        else:
+            my_print(
+                bcolors.GREEN,
+                "No differences found for package %s." % package_name
             )
 
-            # install and print out if the active .whl is compiled or not
-            venv.runCommand(
-                commands=[
-                    "python -m pip install -U %s" % os.path.join(dist_dir, os.listdir(dist_dir)[0]),
-                    "python -c print(getattr(__import__('%s'),'__compiled__','__uncompiled_version__'))" % package_name,
-                ]
-            )
+        my_print(
+            "=================================================================================\n",
+            bcolors.ENDC
+        )
 
-            # get uncompiled pytest results
-            uncompiled_stdout, uncompiled_stderr = venv.runCommandWithOutput(
-                commands=[
-                    "cd %s" % package_name,
-                    "python -m pytest --disable-warnings",
-                ]
-            )
 
-            # build nuitka compiled .whl
-            venv.runCommand(
-                commands=[
-                    "cd %s" % package_name,
-                    "rm -rf dist",
-                    "python setup.py bdist_nuitka",
-                ]
-            )
+        if exit_code != 0 and search_mode.abortOnFinding(dirname=None, filename=package_name):
+            break
 
-            # install and print out if the active .whl is compiled or not
-            venv.runCommand(
-                commands=[
-                    "python -m pip install -U %s" % os.path.join(dist_dir, os.listdir(dist_dir)[0]),
-                    "python -c print(getattr(__import__('%s'),'__compiled__','__uncompiled_version__'))" % package_name,
-                ]
-            )
-
-            # get compiled pytest results
-            compiled_stdout, compiled_stderr = venv.runCommandWithOutput(
-                commands=[
-                    "cd %s" % package_name,
-                    "python -m pytest --disable-warnings",
-                ]
-            )
-
-    except Exception as e:
-        my_print("Package", package_name, "ran into an exception during execution, traceback: ")
-        my_print(e)
         if search_mode.abortIfExecuted():
             break
-        continue
+
+    search_mode.finish()
 
 
-
-    os.chdir(base_dir)
-
-    # TODO: currently not working due to permission errors on removing .git files
-    removeDirectory(os.path.join(base_dir, "venv_%s" % package_name), ignore_errors=False)
-
-    # compare outputs
-    stdout_diff = compareOutput(
-        "stdout",
-        uncompiled_stdout,
-        compiled_stdout,
-        ignore_warnings=True,
-        ignore_infos=True,
-        syntax_errors=True,
-    )
-
-    stderr_diff = compareOutput(
-        "stderr",
-        uncompiled_stderr,
-        compiled_stderr,
-        ignore_warnings=True,
-        ignore_infos=True,
-        syntax_errors=True,
-    )
-
-
-    results.append((package_name,stdout_diff,stderr_diff))
-
-    exit_code = stdout_diff or stderr_diff
-
+    # give a summary of all packages
 
     my_print(
-        bcolors.RED if exit_code else bcolors.GREEN,
-        "\n=================================================================================",
+        bcolors.YELLOW,
+        "\n\n=====================================SUMMARY=====================================",
         bcolors.ENDC
     )
 
-    my_print(
-        bcolors.RED if exit_code else bcolors.GREEN,
-        "--- %s ---" % package_name,
-        "exit_stdout:",
-        stdout_diff,
-        "exit_stderr:",
-        stderr_diff,
-        bcolors.ENDC
-    )
+    for package_name, stdout_diff, stderr_diff in results:
+        my_print(
+            bcolors.RED if (stdout_diff or stderr_diff) else bcolors.GREEN,
+            bcolors.UNDERLINE + package_name + bcolors.ENDC,
+            "-",
 
-    if exit_code:
-        my_print(
-            bcolors.RED,
-            "Error, outputs differed for package %s." % package_name
-        )
-    else:
-        my_print(
-            bcolors.GREEN,
-            "No differences found for package %s." % package_name
+            bcolors.RED if stdout_diff else bcolors.GREEN,
+            "stdout:",
+            stdout_diff,
+            bcolors.ENDC,
+
+            bcolors.RED if stderr_diff else bcolors.GREEN,
+            "stderr:",
+            stderr_diff,
+            bcolors.ENDC,
+
+            "\n---------------------------------------------------------------------------------"
         )
 
-    my_print(
-        "=================================================================================\n",
-        bcolors.ENDC
-    )
 
-
-    if exit_code != 0 and search_mode.abortOnFinding(dirname=None, filename=package_name):
-        break
-
-    if search_mode.abortIfExecuted():
-        break
-
-search_mode.finish()
-
-
-# give a summary of all packages
-
-my_print(
-    bcolors.YELLOW,
-    "\n\n=====================================SUMMARY=====================================",
-    bcolors.ENDC
-)
-
-for package_name, stdout_diff, stderr_diff in results:
-    my_print(
-        bcolors.RED if (stdout_diff or stderr_diff) else bcolors.GREEN,
-        bcolors.UNDERLINE + package_name + bcolors.ENDC,
-        "-",
-
-        bcolors.RED if stdout_diff else bcolors.GREEN,
-        "stdout:",
-        stdout_diff,
-        bcolors.ENDC,
-
-        bcolors.RED if stderr_diff else bcolors.GREEN,
-        "stderr:",
-        stderr_diff,
-        bcolors.ENDC,
-
-        "\n---------------------------------------------------------------------------------"
-    )
+if __name__ == "__main__":
+    main()
