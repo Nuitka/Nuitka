2d043ac4383809cdbce7b12293ba11c36d17c32a
Kay Hayen <kay.hayen@gmail.com>
2019-08-10 22:07:48 +0200

    Tests: Output directories on Windows for failed standalone tests too.

--- tests/standalone/run_all.py
+++ tests/standalone/run_all.py
@@ -104,7 +104,13 @@ def displayError(dirname, filename):
     assert dirname is None
 
     my_print("Listing of dist folder:")
-    os.system("ls -Rla %s" % filename[:-3] + ".dist")
+
+    if os.name == "nt":
+        command = "dir /b /s /a:-D %s"
+    else:
+        command = "ls -Rla %s"
+
+    os.system(command % filename)
 
 
 def main():
