94b35b0133a470b651d780596c09813a62e2c9ff
Tommy Li <tommyli3318@gmail.com>
2019-08-11 14:55:20 -0700

    asn1crypto & attrs

---
+++ tests/standalone/Asn1cryptoUsing.py
@@ -0,0 +1,101 @@
+#     Copyright 2019, Kay Hayen, mailto:kay.hayen@gmail.com
+#
+#     Python test originally created or extracted from other peoples work. The
+#     parts from me are licensed as below. It is at least Free Software where
+#     it's copied from other people. In these cases, that will normally be
+#     indicated.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#
+
+
+# nuitka-skip-unless-imports: asn1crypto
+
+from asn1crypto.core import ObjectIdentifier
+
+class MyType(ObjectIdentifier):
+    _map = {
+        '1.8.2.1.23': 'value_name',
+        '1.8.2.1.24': 'other_value',
+    }
+
+# Will print: "value_name"
+print(MyType('1.8.2.1.23').native)
+
+# Will print: "1.8.2.1.23"
+print(MyType('1.8.2.1.23').dotted)
+
+# Will print: "1.8.2.1.25"
+print(MyType('1.8.2.1.25').native)
+
+# Will print "value_name"
+print(MyType.map('1.8.2.1.23'))
+
+# Will print "1.8.2.1.23"
+print(MyType.unmap('value_name'))
+
+
+from asn1crypto.core import BitString
+b1 = BitString((1, 0, 1))
+
+class MyFlags(BitString):
+    _map = {
+        0: 'edit',
+        1: 'delete',
+        2: 'manage_users',
+    }
+
+permissions = MyFlags({'edit', 'delete'})
+
+if permissions['edit'] and permissions['delete']:
+    print('Can edit and delete')
+
+if 'manage_users' in permissions.native:
+    print('Is admin')
+
+
+from asn1crypto.core import OctetBitString, IntegerBitString
+bit = BitString((1, 0, 1, 1))
+print(bit.native)
+
+octet = bit.cast(OctetBitString)
+
+print(octet.native)
+
+i = bit.cast(IntegerBitString)
+
+print(i.native)
+
+
+from asn1crypto.core import Sequence, Choice, IA5String, UTCTime, ObjectIdentifier
+
+class Person(Choice):
+    _alternatives = [
+        ('name', IA5String),
+        ('email', IA5String, {'implicit': 0}),
+    ]
+
+class Record(Sequence):
+    _fields = [
+        ('id', ObjectIdentifier),
+        ('created', UTCTime),
+        ('creator', Person, {'explicit': 0, 'optional': True}),
+    ]
+
+person = Person(name='email', value='will@wbond.net')
+
+print(person.implicit)
+
+print(person.untag().implicit)
+
+print(person.tag)
---
+++ tests/standalone/AttrsUsing.py
@@ -0,0 +1,106 @@
+#     Copyright 2019, Kay Hayen, mailto:kay.hayen@gmail.com
+#
+#     Python test originally created or extracted from other peoples work. The
+#     parts from me are licensed as below. It is at least Free Software where
+#     it's copied from other people. In these cases, that will normally be
+#     indicated.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#
+
+
+# nuitka-skip-unless-imports: attr
+
+import attr
+
+@attr.s
+class Empty(object):
+    pass
+print(Empty())
+print(Empty() == Empty())
+print(Empty() is Empty())
+
+
+@attr.s
+class Coordinates(object):
+    x = attr.ib()
+    y = attr.ib()
+
+c1 = Coordinates(1, 2)
+print(c1)
+c2 = Coordinates(x=2, y=1)
+print(c2)
+print(c1 == c2)
+
+
+from attr import attrs, attrib
+@attrs
+class SeriousCoordinates(object):
+    x = attrib()
+    y = attrib()
+print(SeriousCoordinates(1, 2))
+print(attr.fields(Coordinates) == attr.fields(SeriousCoordinates))
+
+
+@attr.s
+class A(object):
+    a = attr.ib()
+    def get_a(self):
+        return self.a
+@attr.s
+class B(object):
+    b = attr.ib()
+@attr.s
+class C(A, B):
+    c = attr.ib()
+i = C(1, 2, 3)
+
+print(i)
+
+print(i == C(1, 2, 3))
+
+print(i.get_a())
+
+
+import collections
+@attr.s
+class Connection(object):
+    socket = attr.ib()
+    @classmethod
+    def connect(cls, db_string):
+       # connect somehow to db_string ...
+       return cls(socket=42)
+@attr.s
+class ConnectionPool(object):
+    db_string = attr.ib()
+    pool = attr.ib(default=attr.Factory(collections.deque))
+    debug = attr.ib(default=False)
+    def get_connection(self):
+        try:
+            return self.pool.pop()
+        except IndexError:
+            if self.debug:
+                print("New connection!")
+            return Connection.connect(self.db_string)
+    def free_connection(self, conn):
+        if self.debug:
+            print("Connection returned!")
+        self.pool.appendleft(conn)
+
+
+cp = ConnectionPool("postgres://localhost")
+print(cp)
+conn = cp.get_connection()
+print(conn)
+cp.free_connection(conn)
+print(cp)
