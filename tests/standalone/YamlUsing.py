e7272a49eda1defdb6fecedec30fcccf391c5b18
Tommy Li <tommyli3318@gmail.com>
2019-08-11 14:13:26 -0700

    DateutilUsing.py & YamlUsing.py

---
+++ tests/standalone/YamlUsing.py
@@ -0,0 +1,119 @@
+#     Copyright 2019, Kay Hayen, mailto:kay.hayen@gmail.com
+#
+#     Python test originally created or extracted from other peoples work. The
+#     parts from me are licensed as below. It is at least Free Software where
+#     it's copied from other people. In these cases, that will normally be
+#     indicated.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#
+
+
+# nuitka-skip-unless-imports: yaml
+
+
+import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+
+# test load
+test_str1 = """
+- one
+- two
+- three
+- four
+"""
+
+print(
+	yaml.load(
+		test_str1,
+		Loader=Loader
+	)
+)
+
+
+#test load_all
+test_str2 = """
+---
+name: The Set of Gauntlets 'Pauraegen'
+description: >
+  A set of handgear with sparks that crackle
+  across its knuckleguards.
+---
+name: The Set of Gauntlets 'Paurnen'
+description: >
+  A set of gauntlets that gives off a foul,
+  acrid odour yet remains untarnished.
+---
+name: The Set of Gauntlets 'Paurnimmen'
+description: >
+  A set of handgear, freezing with unnatural cold.
+"""
+
+for data in yaml.load_all(test_str2):
+	print(data)
+
+
+# test load class object
+class Hero:
+    def __init__(self, name, hp, sp):
+        self.name = name
+        self.hp = hp
+        self.sp = sp
+    def __repr__(self):
+        return "%s(name=%r, hp=%r, sp=%r)" % (
+            self.__class__.__name__, self.name, self.hp, self.sp)
+
+
+print(
+	yaml.load("""
+!!python/object:__main__.Hero
+name: Welthyr Syxgon
+hp: 1200
+sp: 0
+""")
+)
+
+
+# test dump
+print(yaml.dump([1,2,3], explicit_start=True))
+print(yaml.dump(Hero("Galain Ysseleg", hp=-3, sp=2)))
+print(yaml.dump(range(5), canonical=True))
+
+
+
+# test yaml.YAMLObject inheritance
+class Monster(yaml.YAMLObject):
+    yaml_tag = u'!Monster'
+    def __init__(self, name, hp, ac, attacks):
+        self.name = name
+        self.hp = hp
+        self.ac = ac
+        self.attacks = attacks
+    def __repr__(self):
+        return "%s(name=%r, hp=%r, ac=%r, attacks=%r)" % (
+            self.__class__.__name__, self.name, self.hp, self.ac, self.attacks)
+
+print(
+	yaml.dump(
+		Monster(
+			name='Cave lizard',
+			hp=[3,6],
+			ac=16,
+			attacks=['BITE','HURT']
+			)
+		)
+	)
