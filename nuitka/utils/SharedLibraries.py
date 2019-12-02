aca388331b6b08fdaf40d3df38be57c079e17e88
gioboske <giorgio.boschetti@gmail.com>
2019-12-02 15:18:34 +0100

    MacOS standalone rpath issue (Feature/fix issue #499) (#579)
    * Add convenient method to add rpath to Mach-O binary
    
    Add convenient method to add rpath to Mach-O binary. The method is derived from `nuitka.utils.SharedLibraries.callInstallNameTool`.
    
    * Update `executePostProcessing` to add rpath for libpython
    
    Update `executePostProcessing` to add rpath for libpython to the Mac-O binary. This is obviously macOS specific.
    
    * Update `executePostProcessing`: add rpath to conda envs only
    
    Add rpath only if executed from conda environments, since the issue #499 seems to affect only conda.

--- nuitka/utils/SharedLibraries.py
+++ nuitka/utils/SharedLibraries.py
@@ -303,3 +303,28 @@ def callInstallNameTool(filename, mapping):
         sys.exit(
             "Error, call to 'install_name_tool' to fix shared library path failed."
         )
+
+def callInstallNameToolAddRPath(filename, rpath):
+    """Adds the rpath path name `rpath` in the specified `filename` Mach-O
+    binary or shared library. If the Mach-O binary already contains the new
+    `rpath` path name, it is an error.
+
+    Args:
+        filename - Mach-O binary or shared library file name.
+        rpath  - rpath path name.
+
+    Returns:
+        None
+
+    Notes:
+        This is obviously macOS specific.
+    """
+    command = ["install_name_tool", "-add_rpath", os.path.join(rpath, '.'), filename]
+
+    with withMadeWritableFileMode(filename):
+        result = subprocess.call(command, stdout=subprocess.PIPE)
+
+    if result != 0:
+        sys.exit(
+            "Error, call to 'install_name_tool' to add rpath failed."
+        )
