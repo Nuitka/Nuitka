b9561e1b095adc5e49927e003e98d421f43aabed
Kay Hayen <kay.hayen@gmail.com>
2020-10-11 16:44:48 +0200

    Quality: Use python binary for autoformat on non-Windows too.
    * Using Python2 is ineffective, therefore we need that now.

--- misc/install-git-hooks.py
+++ misc/install-git-hooks.py
@@ -34,7 +34,10 @@ import stat
 
 from nuitka.tools.Basics import goHome
 from nuitka.utils.Execution import getExecutablePath
-from nuitka.utils.FileOperations import getFileContents, getWindowsShortPathName
+from nuitka.utils.FileOperations import (
+    getFileContents,
+    getWindowsShortPathName,
+)
 
 
 def main():
@@ -89,11 +92,11 @@ please improve this script."""
                     hook_contents[10:],
                 )
 
-                # Also use sys.executable to make sure we find autoformat.
-                hook_contents = hook_contents.replace(
-                    "./bin/autoformat-nuitka-source",
-                    "'%s' ./bin/autoformat-nuitka-source" % sys.executable,
-                )
+            # Also use sys.executable to make sure we find autoformat.
+            hook_contents = hook_contents.replace(
+                "./bin/autoformat-nuitka-source",
+                "'%s' ./bin/autoformat-nuitka-source" % sys.executable,
+            )
         else:
             sys.exit("Error, unknown hook contents.")
 
