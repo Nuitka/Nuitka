8a0886a5c3c767458308f6ae2fc71845290b32cf
Daniel Morgan <danielmorgan0x1@gmail.com>
2020-10-14 13:23:22 +0200

    Added getSharedLibrarySuffix to Utils

--- nuitka/utils/Utils.py
+++ nuitka/utils/Utils.py
@@ -130,3 +130,28 @@ def isExecutableCommand(command):
                 return True
 
     return False
+
+def getSharedLibrarySuffix():
+    if python_version < 300:
+        import imp
+
+        result = None
+
+        for suffix, _mode, module_type in imp.get_suffixes():
+            if module_type != imp.C_EXTENSION:
+                continue
+
+            if result is None or len(suffix) < len(result):
+                result = suffix
+
+        return result
+    else:
+        import importlib.machinery  # pylint: disable=I0021,import-error,no-name-in-module
+
+        result = None
+
+        for suffix in importlib.machinery.EXTENSION_SUFFIXES:
+            if result is None or len(suffix) < len(result):
+                result = suffix
+
+        return result
