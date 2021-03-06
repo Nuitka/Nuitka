66e71c52a9e033f856cdd55a3a23a12601079cfa
Kay Hayen <kay.hayen@gmail.com>
2021-03-06 20:18:34 +0100

    Added experimental ability to make constant data writable

--- nuitka/build/SconsCompilerSettings.py
+++ nuitka/build/SconsCompilerSettings.py
@@ -221,6 +221,9 @@ def addConstantBlobFile(
 #define INCBIN_PREFIX
 #define INCBIN_STYLE INCBIN_STYLE_SNAKE
 #define INCBIN_LOCAL
+#ifdef _NUITKA_EXPERIMENTAL_WRITEABLE_CONSTANTS
+#define INCBIN_OUTPUT_SECTION ".data"
+#endif
 
 #include "nuitka/incbin.h"
 
