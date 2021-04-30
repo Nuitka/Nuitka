0c88a3c7fa0f79613d09b998d3151ace3601312e
mammo0 <mammo0@users.noreply.github.com>
2021-04-30 08:00:25 +0200

    Standalone: added 'pandas._libs.window' to 'pandas.core.window' (#1078)
    * This should fix "ModuleNotFoundError: No module named  'pandas._libs.window'"

--- nuitka/plugins/standard/ImplicitImports.py
+++ nuitka/plugins/standard/ImplicitImports.py
@@ -931,6 +931,7 @@ class NuitkaPluginPopularImplicitImports(NuitkaPluginBase):
             yield "pandas._libs.tslibs.nattype"
             yield "pandas._libs.tslibs.base"
         elif full_name == "pandas.core.window":
+            yield "pandas._libs.window"
             yield "pandas._libs.skiplist"
         elif full_name == "pandas._libs.testing":
             yield "cmath"
