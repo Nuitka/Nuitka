d8fe7fa8e54cdeedcf9ce35380aefb06e04f54ad
Kay Hayen <kay.hayen@gmail.com>
2021-07-02 09:02:11 +0200

    Optimization: Use proper type to avoid conversions

--- nuitka/build/static_src/HelpersHeapStorage.c
+++ nuitka/build/static_src/HelpersHeapStorage.c
@@ -15,11 +15,17 @@
 //     See the License for the specific language governing permissions and
 //     limitations under the License.
 //
-/** For making yield and yield from capable of persisting C stack.
+/** For making "yield" and "yield from" capable of persisting current C stack.
  *
  * These copy objects pointed to into an array foreseen for this task.
  *
  **/
+
+// This file is included from another C file, help IDEs to still parse it on
+// its own.
+#ifdef __IDE_ONLY__
+#include "nuitka/prelude.h"
+#endif
 
 void Nuitka_PreserveHeap(void *dest, ...) {
     va_list(ap);
@@ -29,10 +35,11 @@ void Nuitka_PreserveHeap(void *dest, ...) {
 
     for (;;) {
         void *source = va_arg(ap, void *);
-        if (source == NULL)
+        if (source == NULL) {
             break;
+        }
 
-        ssize_t size = va_arg(ap, ssize_t);
+        size_t size = va_arg(ap, size_t);
         memcpy(w, source, size);
         w += size;
     }
@@ -46,10 +53,11 @@ void Nuitka_RestoreHeap(void *source, ...) {
 
     for (;;) {
         void *dest = va_arg(ap, void *);
-        if (dest == NULL)
+        if (dest == NULL) {
             break;
+        }
 
-        ssize_t size = va_arg(ap, ssize_t);
+        size_t size = va_arg(ap, size_t);
         memcpy(dest, w, size);
         w += size;
     }
