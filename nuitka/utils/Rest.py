672da8e0a644273da39207422aa8679f292f2724
Kay Hayen <kay.hayen@gmail.com>
2021-12-23 09:04:22 +0100

    Rest: Added utility helper to create tables
    * For use in the website generation.

--- nuitka/utils/Rest.py
+++ nuitka/utils/Rest.py
@@ -15,19 +15,18 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 #
-""" Create PDF documentations.
+""" Work with ReST documentations.
 
-This ought to be in "nuitka.tools.release" but the packaging might happen
-on release package, as in case of Debian, therefore it's here.
+This e.g. creates PDF documentations during release and tables from data
+for the web site, e.g. downloads.
 
 """
 
+import functools
 import os
 import tempfile
 
-from nuitka.utils.Execution import withEnvironmentVarOverriden
-
-from .Execution import check_call
+from .Execution import check_call, withEnvironmentVarOverriden
 from .FileOperations import (
     changeFilenameExtension,
     deleteFile,
@@ -103,3 +102,37 @@ def createPDF(document):
     assert os.path.exists(pdf_filename)
 
     return pdf_filename
+
+
+def makeTable(grid):
+    """Create a REST table."""
+
+    def makeSeparator(num_cols, col_width, header_flag):
+        if header_flag == 1:
+            return num_cols * ("+" + (col_width) * "=") + "+\n"
+        else:
+            return num_cols * ("+" + (col_width) * "-") + "+\n"
+
+    def normalizeCell(string, length):
+
+        return string + ((length - len(string)) * " ")
+
+    cell_width = 2 + max(
+        functools.reduce(
+            lambda x, y: x + y, [[len(item) for item in row] for row in grid], []
+        )
+    )
+    num_cols = len(grid[0])
+    rst = makeSeparator(num_cols, cell_width, 0)
+    header_flag = 1
+    for row in grid:
+        rst = (
+            rst
+            + "| "
+            + "| ".join([normalizeCell(x, cell_width - 1) for x in row])
+            + "|\n"
+        )
+        rst = rst + makeSeparator(num_cols, cell_width, header_flag)
+        header_flag = 0
+
+    return rst
