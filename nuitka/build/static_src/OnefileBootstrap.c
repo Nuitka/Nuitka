06db7a988318e715630dc7b9e591bb63f346e3fd
Kay Hayen <kay.hayen@gmail.com>
2022-04-01 12:18:54 +0200

    Onefile: Release memory buffers from decompression.

--- nuitka/build/static_src/OnefileBootstrap.c
+++ nuitka/build/static_src/OnefileBootstrap.c
@@ -238,27 +238,34 @@ static FILE_HANDLE exe_file;
 
 #ifdef _NUITKA_ONEFILE_COMPRESSION
 
-static ZSTD_DCtx *dctx = NULL;
+static ZSTD_DCtx *dest_ctx = NULL;
 static ZSTD_inBuffer input = {NULL, 0, 0};
 static ZSTD_outBuffer output = {NULL, 0, 0};
 
 static void initZSTD(void) {
-    size_t const buffInSize = ZSTD_DStreamInSize();
-    input.src = malloc(buffInSize);
+    size_t const input_buffer_size = ZSTD_DStreamInSize();
+    input.src = malloc(input_buffer_size);
     if (input.src == NULL) {
         fatalErrorMemory();
     }
 
-    size_t const buffOutSize = ZSTD_DStreamOutSize();
-    output.dst = malloc(buffOutSize);
+    size_t const output_buffer_size = ZSTD_DStreamOutSize();
+    output.dst = malloc(output_buffer_size);
     if (output.dst == NULL) {
         fatalErrorMemory();
     }
 
-    dctx = ZSTD_createDCtx();
-    if (dctx == NULL) {
+    dest_ctx = ZSTD_createDCtx();
+    if (dest_ctx == NULL) {
         fatalErrorMemory();
     }
+}
+
+static void releaseZSTD(void) {
+    ZSTD_freeDCtx(dest_ctx);
+
+    free((void *)input.src);
+    free(output.dst);
 }
 
 #endif
@@ -334,7 +341,7 @@ static void readPayloadChunk(void *buffer, size_t size) {
             output.pos = 0;
             output.size = ZSTD_DStreamOutSize();
 
-            size_t const ret = ZSTD_decompressStream(dctx, &output, &input);
+            size_t const ret = ZSTD_decompressStream(dest_ctx, &output, &input);
             // printf("return output %d %d\n", output.pos, output.size);
             end_of_buffer = (output.pos == output.size);
 
@@ -846,7 +853,6 @@ int main(int argc, char **argv) {
 
                 *w = FILENAME_SEP_CHAR;
 
-                // _putws(target_path);
                 createDirectory(target_path);
             }
 
@@ -893,6 +899,10 @@ int main(int argc, char **argv) {
     }
 
     closeFile(exe_file);
+
+#ifdef _NUITKA_ONEFILE_COMPRESSION
+    releaseZSTD();
+#endif
 
     // Pass our pid by value to the child. If we exit for some reason, re-parenting
     // might change it by the time the child looks at its parent.
