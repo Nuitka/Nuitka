2995433bf6874a8fad2528e944343097332988b5
Kay Hayen <kay.hayen@gmail.com>
2022-06-20 11:44:13 +0200

    Optimization: Also do calls through variables for constants and builtins
    * For constants these are errors, but for builtins they can make a difference.

--- nuitka/nodes/BuiltinRefNodes.py
+++ nuitka/nodes/BuiltinRefNodes.py
@@ -150,6 +150,16 @@ class ExpressionBuiltinRef(ExpressionBuiltinRefBase):
 
         return new_node, tags, message
 
+    def computeExpressionCallViaVariable(
+        self, call_node, variable_ref_node, call_args, call_kw, trace_collection
+    ):
+        return self.computeExpressionCall(
+            call_node=call_node,
+            call_args=call_args,
+            call_kw=call_kw,
+            trace_collection=trace_collection,
+        )
+
     @staticmethod
     def isKnownToBeIterable(count):
         # TODO: Why yes, some may be, could be told here.
@@ -260,3 +270,13 @@ class ExpressionBuiltinExceptionRef(ExpressionBuiltinRefBase):
         assert new_node is not None
 
         return new_node, "new_expression", "Detected built-in exception making."
+
+    def computeExpressionCallViaVariable(
+        self, call_node, variable_ref_node, call_args, call_kw, trace_collection
+    ):
+        return self.computeExpressionCall(
+            call_node=call_node,
+            call_args=call_args,
+            call_kw=call_kw,
+            trace_collection=trace_collection,
+        )
