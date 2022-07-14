7ee4ac0d278ea8a2f2118b861e5d321346f86bbd
Kay Hayen <kay.hayen@gmail.com>
2022-07-14 12:23:15 +0200

    Optimization: Faster binary operation with constant values
    * This is mostly for Python3 "int" values, as Python2 "int" was
      already covered previously.
    
    * This cleanups how comparison code abstracts the long value
      type family and makes it reusable for the binary operation
      slots.
    
    * Not yet any effective changes though.

--- nuitka/build/static_src/HelpersComparisonGe.c
+++ nuitka/build/static_src/HelpersComparisonGe.c
@@ -11555,7 +11555,6 @@ nuitka_bool RICH_COMPARE_GE_NBOOL_LIST_LIST(PyObject *operand1, PyObject *operan
 }
 
 #if PYTHON_VERSION < 0x300
-
 static PyObject *COMPARE_GE_OBJECT_LONG_CLONG(PyObject *operand1, long operand2) {
     CHECK_OBJECT(operand1);
     assert(PyLong_CheckExact(operand1));
@@ -11620,7 +11619,6 @@ PyObject *RICH_COMPARE_GE_OBJECT_LONG_INT(PyObject *operand1, PyObject *operand2
 #endif
 
 #if PYTHON_VERSION < 0x300
-
 static bool COMPARE_GE_CBOOL_LONG_CLONG(PyObject *operand1, long operand2) {
     CHECK_OBJECT(operand1);
     assert(PyLong_CheckExact(operand1));
