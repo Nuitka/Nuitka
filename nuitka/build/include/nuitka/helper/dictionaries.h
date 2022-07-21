22800591156606ef8c23199458898a5bfa04252a
Maxwell175 <admin@mdtech.us>
2022-07-21 12:47:36 +0200

    NoGil: Follow some of the API changes
    * GIL check needs a change
    
    * Split dictionary tables are no more.
    
    * Use "Py_SET_REFCNT" everywhere, as "Py_REFCNT" is not writable anymore.

--- nuitka/build/include/nuitka/helper/dictionaries.h
+++ nuitka/build/include/nuitka/helper/dictionaries.h
@@ -179,8 +179,10 @@ static Nuitka_DictEntryHandle GET_STRING_DICT_ENTRY(PyDictObject *dict, Nuitka_S
 
     if (value == NULL) {
         return NULL;
+#ifndef _NUITKA_EXPERIMENTAL_NOGIL
     } else if (_PyDict_HasSplitTable(dict)) {
         return &dict->ma_values[ix];
+#endif
     } else {
         return &DK_ENTRIES(dict->ma_keys)[ix].me_value;
     }
