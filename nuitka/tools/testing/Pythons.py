86d7cba8a03083a8cd95b9d0f06c729b10a6c704
Kay Hayen <kay.hayen@gmail.com>
2022-07-25 09:12:20 +0200

    Tests: Make installed Pythons and function to execute with available

---
+++ nuitka/tools/testing/Pythons.py
@@ -0,0 +1,33 @@
+#     Copyright 2022, Kay Hayen, mailto:kay.hayen@gmail.com
+#
+#     Part of "Nuitka", an optimizing Python compiler that is compatible and
+#     integrates with CPython, but also works on its own.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#
+""" Test tool to run a program with various Pythons. """
+
+
+from nuitka.PythonVersions import getSupportedPythonVersions
+from nuitka.utils.Execution import check_output
+from nuitka.utils.InstalledPythons import findPythons
+
+
+def findAllPythons():
+    for python_version in getSupportedPythonVersions():
+        for python in findPythons(python_version):
+            yield python, python_version
+
+
+def executeWithInstalledPython(python, args):
+    return check_output([python.getPythonExe()] + args)
