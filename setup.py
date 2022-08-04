b22fccf62661864ee48b9ea1d4001a34844dd3b1
Kay Hayen <kay.hayen@gmail.com>
2022-08-04 12:51:10 +0200

    Minor cleanup

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ the source code. Second, the packages are scanned from the filesystem,
 and third, the byte code compilation is avoided for inline copies of
 scons with mismatching Python major versions.
 
-spellchecker: ignore chdir,pythonw,tqdm
+spellchecker: ignore chdir,pythonw,tqdm,distutil,atomicwrites,markupsafe,wininst,distclass
 """
 import os
 import sys
