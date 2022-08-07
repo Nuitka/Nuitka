2a2e9323209d6db7a2c612b26fe528b0a81441a2
Kay Hayen <kay.hayen@gmail.com>
2022-08-07 14:27:12 +0200

    Plugins: Fixed sorting and newlines in Yaml auto-format
    * Also added first import hacks definitions, but there
      is not actual use yet.

--- nuitka/tools/quality/auto_format/YamlFormatter.py
+++ nuitka/tools/quality/auto_format/YamlFormatter.py
@@ -36,6 +36,7 @@ ANTI_BLOAT_KEYS = None
 IMPLICIT_IMPORTS_KEYS = None
 OPTIONS_KEYS = None
 OPTIONS_CHECKS_KEYS = None
+IMPORT_HACK_KEYS = None
 
 YAML_HEADER = """\
 # yamllint disable rule:line-length
@@ -49,7 +50,7 @@ def _initNuitkaPackageSchema():
     # Singleton, pylint: disable=global-statement
     global MASTER_KEYS, DATA_FILES_KEYS, DLLS_KEYS, DLLS_BY_CODE_KEYS
     global DLLS_FROM_FILENAMES_KEYS, ANTI_BLOAT_KEYS, IMPLICIT_IMPORTS_KEYS
-    global OPTIONS_KEYS, OPTIONS_CHECKS_KEYS
+    global OPTIONS_KEYS, OPTIONS_CHECKS_KEYS, IMPORT_HACK_KEYS
 
     with openTextFile(
         getYamlPackageConfigurationSchemaFilename(),
@@ -86,6 +87,9 @@ def _initNuitkaPackageSchema():
             "properties"
         ].keys()
     )
+    IMPORT_HACK_KEYS = tuple(
+        schema["items"]["properties"]["import-hacks"]["items"]["properties"].keys()
+    )
 
 
 def _decideStrFormat(string):
@@ -102,6 +106,7 @@ def _decideStrFormat(string):
         and string not in ANTI_BLOAT_KEYS
         and string not in IMPLICIT_IMPORTS_KEYS
         and string not in OPTIONS_KEYS
+        and string not in IMPORT_HACK_KEYS
         and string not in OPTIONS_CHECKS_KEYS
     ):
         single_quote_left = string.find("'")
@@ -134,6 +139,47 @@ def _decideStrFormat(string):
 
     else:
         return ""
+
+
+def _reorderDictionary(entry, key_order):
+    import ruamel
+
+    # Yes, friends with ruamel here, to make a sorted copy
+    # pylint: disable=protected-access
+
+    result = ruamel.yaml.comments.CommentedMap()
+    for key, value in sorted(
+        entry._items(),
+        key=lambda item: key_order.index(item[0]) if item[0] in key_order else 1000,
+    ):
+        result[key] = value
+
+        # Strip trailing new lines from end of sequence. It is attached to
+        # the last key.
+        if type(value) is ruamel.yaml.comments.CommentedMap and value.items():
+            sub_mapping_key, _submapping_value = list(value._items())[-1]
+
+            if sub_mapping_key in value.ca.items:
+                ca_value = value.ca.items[sub_mapping_key]
+
+                if type(ca_value[2]) is ruamel.yaml.tokens.CommentToken:
+                    ca_value[2]._value = ca_value[2]._value.rstrip() + "\n"
+
+    entry.copy_attributes(result)
+
+    return result
+
+
+def _reorderDictionaryList(entry_list, key_order):
+    import ruamel
+
+    result = ruamel.yaml.comments.CommentedSeq()
+    result.extend(_reorderDictionary(entry, key_order) for entry in entry_list)
+
+    for attribute_name in entry_list.ca.__slots__:
+        setattr(result.ca, attribute_name, getattr(entry_list.ca, attribute_name))
+
+    return result
 
 
 def formatYaml(path):
@@ -241,68 +287,41 @@ def formatYaml(path):
 
     new_data = []
     for entry in data:
-        sorted_entry = dict(
-            (key, value)
-            for key, value in sorted(
-                entry.items(),
-                key=lambda item: MASTER_KEYS.index(item[0])
-                if item[0] in MASTER_KEYS
-                else 1000,
-            )
-        )
+        sorted_entry = entry  # _reorderDictionary(entry, MASTER_KEYS)
 
         if "data-files" in sorted_entry:
-            sorted_entry["data-files"] = dict(
-                [
-                    (key, entry["data-files"][key])
-                    for key in DATA_FILES_KEYS
-                    if key in entry["data-files"].keys()
-                ]
+            sorted_entry["data-files"] = _reorderDictionary(
+                entry["data-files"], DATA_FILES_KEYS
             )
 
         if "dlls" in sorted_entry:
-            for sub_entry in sorted_entry["dlls"]:
-                sub_entry = dict(
-                    [
-                        (key, sub_entry[key])
-                        for key in DLLS_KEYS
-                        if key in sub_entry.keys()
-                    ]
-                )
+            sorted_entry["dlls"] = _reorderDictionaryList(
+                sorted_entry["dlls"], DLLS_KEYS
+            )
 
         if "anti-bloat" in sorted_entry:
-            for sub_entry in sorted_entry["anti-bloat"]:
-                sub_entry = dict(
-                    [
-                        (key, sub_entry[key])
-                        for key in ANTI_BLOAT_KEYS
-                        if key in sub_entry.keys()
-                    ]
-                )
+            sorted_entry["anti-bloat"] = _reorderDictionaryList(
+                sorted_entry["anti-bloat"], ANTI_BLOAT_KEYS
+            )
 
         if "implicit-imports" in sorted_entry:
-            for sub_entry in sorted_entry["implicit-imports"]:
-                sub_entry = dict(
-                    [
-                        (key, sub_entry[key])
-                        for key in IMPLICIT_IMPORTS_KEYS
-                        if key in sub_entry.keys()
-                    ]
-                )
+            sorted_entry["implicit-imports"] = _reorderDictionaryList(
+                sorted_entry["implicit-imports"], IMPLICIT_IMPORTS_KEYS
+            )
 
         if "options" in sorted_entry:
-            for sub_entry in sorted_entry["options"]["checks"]:
-                sub_entry = dict(
-                    [
-                        (key, sub_entry[key])
-                        for key in OPTIONS_CHECKS_KEYS
-                        if key in sub_entry.keys()
-                    ]
-                )
+            sorted_entry["options"]["checks"] = _reorderDictionaryList(
+                sorted_entry["options"]["checks"], OPTIONS_CHECKS_KEYS
+            )
+
+        if "import-hacks" in sorted_entry:
+            sorted_entry["import-hacks"] = _reorderDictionaryList(
+                sorted_entry["import-hacks"], IMPORT_HACK_KEYS
+            )
 
         new_data.append(sorted_entry)
 
-    new_data = sorted(new_data, key=lambda d: d["module-name"].lower())
+    # new_data = sorted(new_data, key=lambda d: d["module-name"].lower())
 
     with open(path, "w", encoding="utf-8") as output_file:
         output_file.write(YAML_HEADER)
