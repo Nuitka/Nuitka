68a52dc6cae61df27c687f6e43fc13124081156f
Kay Hayen <kay.hayen@gmail.com>
2022-11-20 18:01:54 +0100

    Tests: Ignore Python3.11 related super error inconsistency

--- nuitka/tools/testing/OutputComparison.py
+++ nuitka/tools/testing/OutputComparison.py
@@ -225,6 +225,15 @@ exceeded while calling a Python object' in \
         if "clcache: persistent json file" in line or "clcache: manifest file" in line:
             continue
 
+        # Ignore manual error message of CPython 3.11 that is different from the generic one for super
+        line = line.replace(
+            "super() argument 1 must be a type, not NoneType",
+            "super() argument 1 must be type, not None",
+        )
+        line = line.replace(
+            "super() argument 1 must be a type", "super() argument 1 must be type"
+        )
+
         result.append(line)
 
     return result
