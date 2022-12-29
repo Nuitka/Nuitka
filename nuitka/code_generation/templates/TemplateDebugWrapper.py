07871ffbd4bdc719824fbbf7add1c5ab2fd92244
Kay Hayen <kay.hayen@gmail.com>
2022-12-29 10:06:35 +0100

    Cleanup, unified code generation for "once" and "full" frame modes
    * Caching is a separate decision than object type, so now this
      could not cache for class frames, which is the goal.
    
    * Changed templates to Jinja2 templates while at it, so we avoid
      some code clutter to the C pre-processor, nothing too important
      though.

--- nuitka/code_generation/templates/TemplateDebugWrapper.py
+++ nuitka/code_generation/templates/TemplateDebugWrapper.py
@@ -26,44 +26,43 @@ from nuitka.__past__ import iterItems
 from nuitka.Tracing import optimization_logger
 
 
+class TemplateWrapper(object):
+    """Wrapper around templates.
+
+    To better trace and control template usage.
+
+    """
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+    def __add__(self, other):
+        return self.__class__(self.name + "+" + other.name, self.value + other.value)
+
+    def __mod__(self, other):
+        assert type(other) is dict, self.name
+
+        for key in other.keys():
+            if "%%(%s)" % key not in self.value:
+                optimization_logger.warning(
+                    "Extra value %r provided to template %r." % (key, self.name)
+                )
+
+        try:
+            return self.value % other
+        except KeyError as e:
+            raise KeyError(self.name, *e.args)
+
+    def split(self, sep):
+        return self.value.split(sep)
+
+
 def enableDebug(globals_dict):
     templates = dict(globals_dict)
-
-    class TemplateWrapper(object):
-        """Wrapper around templates.
-
-        To better trace and control template usage.
-
-        """
-
-        def __init__(self, name, value):
-            self.name = name
-            self.value = value
-
-        def __str__(self):
-            return self.value
-
-        def __add__(self, other):
-            return self.__class__(
-                self.name + "+" + other.name, self.value + other.value
-            )
-
-        def __mod__(self, other):
-            assert type(other) is dict, self.name
-
-            for key in other.keys():
-                if "%%(%s)" % key not in self.value:
-                    optimization_logger.warning(
-                        "Extra value %r provided to template %r." % (key, self.name)
-                    )
-
-            try:
-                return self.value % other
-            except KeyError as e:
-                raise KeyError(self.name, *e.args)
-
-        def split(self, sep):
-            return self.value.split(sep)
 
     for template_name, template_value in iterItems(templates):
         # Ignore internal attribute like "__name__" that the module will also
@@ -71,7 +70,7 @@ def enableDebug(globals_dict):
         if template_name.startswith("_"):
             continue
 
-        if type(template_value) is str:
+        if type(template_value) is str and "{%" not in template_value:
             globals_dict[template_name] = TemplateWrapper(template_name, template_value)
 
 
