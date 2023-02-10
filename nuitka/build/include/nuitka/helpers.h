71ecc175ddcf67e25be1400ca69edc12ad358e31
Kay Hayen <kay.hayen@gmail.com>
2023-02-10 07:21:38 +0100

    Python3.9+: Fix, resource files objects "iterdir" implemented.

--- nuitka/build/include/nuitka/helpers.h
+++ nuitka/build/include/nuitka/helpers.h
@@ -186,6 +186,13 @@ extern PyObject *OS_PATH_FILE_ISDIR(PyObject *filename);
 // Small helper to list a directory.
 extern PyObject *OS_LISTDIR(PyObject *path);
 
+// Platform standard slash for filenames
+#if defined(_WIN32)
+#define const_platform_sep const_str_backslash
+#else
+#define const_platform_sep const_str_slash
+#endif
+
 // Small helpers to work with filenames.
 extern PyObject *OS_PATH_BASENAME(PyObject *filename);
 
