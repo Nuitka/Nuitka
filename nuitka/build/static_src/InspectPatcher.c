5bae7e609315303312f7b1a41abd2701c1229da9
Kay Hayen <kay.hayen@gmail.com>
2023-02-12 10:47:36 +0100

    Python3.11: Fix, for out tracebacks to have lineno
    * It seems 0 is a valid offset for execution, and -1 is a better choice.

--- nuitka/build/static_src/InspectPatcher.c
+++ nuitka/build/static_src/InspectPatcher.c
@@ -313,7 +313,7 @@ PyTracebackObject *MAKE_TRACEBACK(struct Nuitka_FrameObject *frame, int lineno) 
     result->tb_frame = (PyFrameObject *)frame;
     Py_INCREF(frame);
 
-    result->tb_lasti = 0;
+    result->tb_lasti = -1;
     result->tb_lineno = lineno;
 
     Nuitka_GC_Track(result);
