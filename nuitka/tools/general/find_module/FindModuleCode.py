444d07e47ac9d0723143da1982b4bcedd4903dcd
Kay Hayen <kay.hayen@gmail.com>
2023-05-18 16:32:06 +0200

    UI: Make --edit-module-code work with non-Windows standalone dist paths too
    * Also allow providing non-dist filenames as long as they exist.

--- nuitka/tools/general/find_module/FindModuleCode.py
+++ nuitka/tools/general/find_module/FindModuleCode.py
@@ -44,8 +44,15 @@ def findModuleCode(module_name):
 
 
 def editModuleCode(module_search_desc):
+    # plenty of checks to resolved, pylint: disable=too-many-branches
+
+    module_filename = None
+    module_name = None
+
     if isWin32Windows() and "\\" in module_search_desc:
-        if not os.path.exists(module_search_desc):
+        if os.path.exists(module_search_desc):
+            module_filename = module_search_desc
+        else:
             if module_search_desc.endswith(".py"):
                 module_search_desc = module_search_desc[:-3]
 
@@ -57,12 +64,25 @@ def editModuleCode(module_search_desc):
             module_name = relpath(module_search_desc, start=candidate).replace(
                 "\\", "."
             )
+    elif not isWin32Windows() and "/" in module_search_desc:
+        if os.path.exists(module_search_desc):
+            module_filename = module_search_desc
+        else:
+            if module_search_desc.endswith(".py"):
+                module_search_desc = module_search_desc[:-3]
+
+            candidate = module_search_desc
+
+            while not candidate.endswith(".dist"):
+                candidate = os.path.dirname(candidate)
+
+            module_name = relpath(module_search_desc, start=candidate).replace("/", ".")
     else:
         module_name = ModuleName(module_search_desc)
 
-    addMainScriptDirectory(os.getcwd())
-
-    module_filename = findModuleCode(module_name)
+    if module_name is not None:
+        addMainScriptDirectory(os.getcwd())
+        module_filename = findModuleCode(module_name)
 
     if module_filename is None:
         tools_logger.sysexit("Error, did not find '%s' module" % module_name)
@@ -78,7 +98,8 @@ def editModuleCode(module_search_desc):
                 "Error, %s is a namespace package with no code" % module_name
             )
 
-        tools_logger.info("Found '%s' as '%s'" % (module_name, module_filename))
+        if module_name is not None:
+            tools_logger.info("Found '%s' as '%s'" % (module_name, module_filename))
 
         visual_code_binary = getExecutablePath(
             "code.cmd" if isWin32Windows() else "code"
