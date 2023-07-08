b10b6290ca754ee70b06b1ae6df64eee7892e18b
Kay Hayen <kay.hayen@gmail.com>
2023-07-08 18:26:36 +0200

    Optimization: Statically optimize the value of "sys.byteorder" as well.

--- nuitka/nodes/ImportNodes.py
+++ nuitka/nodes/ImportNodes.py
@@ -184,6 +184,7 @@ module_sys_trust = {
     "hexversion": trust_constant,
     "platform": trust_constant,
     "maxsize": trust_constant,
+    "byteorder": trust_constant,
     "builtin_module_names": trust_constant,
     "stdout": trust_exist,
     "stderr": trust_exist,
