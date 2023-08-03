09ba4cbe798ccd73a794bdf6ed7819322665e40c
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2023-08-03 13:49:59 +1000

    Fix Haiku support in HelpersFilesystemPaths.c
    changed #if defined(__MSYS__) to #if defined(__MSYS__) || defined(__HAIKU__) for sendfile.

--- nuitka/build/static_src/HelpersFilesystemPaths.c
+++ nuitka/build/static_src/HelpersFilesystemPaths.c
@@ -182,7 +182,7 @@ int64_t getFileSize(FILE_HANDLE file_handle) {
 #if defined(__APPLE__)
 #include <copyfile.h>
 #else
-#if defined(__MSYS__)
+#if defined(__MSYS__) || defined(__HAIKU__)
 static bool sendfile(int output_file, int input_file, off_t *bytesCopied, size_t count) {
     char buffer[32768];
 
