bac73eae0090563aa0c19a569cb0486002337661
Florian Sesser <florian.sesser@web.de>
2023-10-11 07:55:33 +0000

    Standalone: Add GI extra dlls: soup, secret
    * With this MSYS2 and GI are working more often.

--- nuitka/plugins/standard/GiPlugin.py
+++ nuitka/plugins/standard/GiPlugin.py
@@ -86,20 +86,23 @@ if not os.environ.get("GI_TYPELIB_PATH"):
 
     @standalone_only
     def getExtraDlls(self, module):
-        if module.getFullName() == "gi._gi":
-            gtk_dll_name = "gtk-3"
-
+        def tryLocateAndLoad(dll_name):
             # Support various name forms in MSYS2 over time.
-            gtk_dll_path = self.locateDLL(gtk_dll_name)
-            if gtk_dll_path is None:
-                gtk_dll_path = self.locateDLL("%s-0" % gtk_dll_name)
-            if gtk_dll_path is None:
-                gtk_dll_path = self.locateDLL("lib%s-0" % gtk_dll_name)
+            dll_path = self.locateDLL(dll_name)
+            if dll_path is None:
+                dll_path = self.locateDLL("%s" % dll_name)
+            if dll_path is None:
+                dll_path = self.locateDLL("lib%s" % dll_name)
 
-            if gtk_dll_path is not None:
+            if dll_path is not None:
                 yield self.makeDllEntryPoint(
-                    source_path=gtk_dll_path,
-                    dest_path=os.path.basename(gtk_dll_path),
+                    source_path=dll_path,
+                    dest_path=os.path.basename(dll_path),
                     package_name=None,
                     reason="needed by 'gi._gi'",
                 )
+
+        if module.getFullName() == "gi._gi":
+            # TODO: Get local relevant DLL names from GI
+            for dll in ["gtk-3-0", "soup-2.4-1", "soup-gnome-2.4-1", "libsecret-1-0"]:
+                yield tryLocateAndLoad(dll)
