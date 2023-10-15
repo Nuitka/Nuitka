91bc2af6ee943063e84511c86ef2dc83e96fd24d
Kay Hayen <kay.hayen@gmail.com>
2023-10-15 11:36:30 +0200

    Minor cleanup

--- nuitka/utils/AppDirs.py
+++ nuitka/utils/AppDirs.py
@@ -73,7 +73,7 @@ def getCacheDir():
                 """\
 Error, failed to create cache directory '%s'. If this is due to a special environment, \
 please consider making a PR for a general solution that adds support for it, or use \
-NUITKA_CACHE_DIR set to a writable directory."""
+'NUITKA_CACHE_DIR' set to a writable directory."""
                 % _cache_dir
             )
 
