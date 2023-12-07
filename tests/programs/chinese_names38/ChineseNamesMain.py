4b016ff01594a150de825bb1654032bc8680cc80
Kay Hayen <kay.hayen@gmail.com>
2023-12-07 01:49:28 +0100

    Tests: Make this directly executable

--- tests/programs/chinese_names38/ChineseNamesMain.py
+++ tests/programs/chinese_names38/ChineseNamesMain.py
@@ -16,6 +16,8 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 #
+# nuitka-project: --follow-import-to=测试
+# nuitka-project: --follow-import-to=那里
 
 from 测试 import 这里
 
