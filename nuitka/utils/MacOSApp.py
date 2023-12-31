399793114d60c80b4aebebbcb9ad205a373b6af4
Kay Hayen <kay.hayen@gmail.com>
2023-12-31 08:36:25 +0100

    Cleanup, separate out icon accessor functions to platform specific ones

--- nuitka/utils/MacOSApp.py
+++ nuitka/utils/MacOSApp.py
@@ -62,7 +62,7 @@ def createPlistInfoFile(logger, onefile):
         ]
     )
 
-    icon_paths = Options.getIconPaths()
+    icon_paths = Options.getMacOSIconPaths()
 
     if icon_paths:
         assert len(icon_paths) == 1
