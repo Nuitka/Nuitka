a39d6f04607117cbb73c72535af001639905cd42
Kay Hayen <kay.hayen@gmail.com>
2024-01-05 13:34:30 +0100

    Optimization: Avoid importing "setuptools_scm" for "tqdm" inline copy
    * This gives a warning on at least Ubuntu

--- nuitka/build/inline_copy/tqdm/tqdm/version.py
+++ nuitka/build/inline_copy/tqdm/tqdm/version.py
@@ -2,8 +2,4 @@
 try:
     from ._dist_ver import __version__
 except ImportError:
-    try:
-        from setuptools_scm import get_version
-        __version__ = get_version(root='..', relative_to=__file__)
-    except (ImportError, LookupError):
-        __version__ = "UNKNOWN"
+    __version__ = "UNKNOWN"
