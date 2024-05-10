7064035b09d173e1e8d07396e3b086fe52fcf83c
Kay Hayen <kay.hayen@gmail.com>
2024-05-10 11:56:35 +0200

    Python3.12: Fix, providing the context for runtime errors was disabled
    * These functions used work correctly now

--- nuitka/build/static_src/CompiledGeneratorType.c
+++ nuitka/build/static_src/CompiledGeneratorType.c
@@ -481,13 +481,11 @@ static void RAISE_RUNTIME_ERROR_RAISED_STOP_ITERATION(PyThreadState *tstate, cha
                         &new_exception_state.exception_tb);
 #endif
 
-#if PYTHON_VERSION < 0x3c0
     Py_INCREF(saved_exception_state.exception_value);
     RAISE_EXCEPTION_WITH_CAUSE_STATE(tstate, &new_exception_state, saved_exception_state.exception_value);
 
     Py_INCREF(saved_exception_state.exception_value);
     PyException_SetContext(new_exception_state.exception_value, saved_exception_state.exception_value);
-#endif
 
     RELEASE_ERROR_OCCURRED_STATE_X(&saved_exception_state);
     RESTORE_ERROR_OCCURRED_STATE(tstate, &new_exception_state);
