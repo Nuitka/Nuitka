375d631d34dabd7a541da9a4ee5d9aa55b5cac1f
Kay Hayen <kay.hayen@gmail.com>
2024-05-22 21:40:42 +0200

    UI: Do not output module ast when a plugin reports an error

--- nuitka/tree/TreeHelpers.py
+++ nuitka/tree/TreeHelpers.py
@@ -291,6 +291,8 @@ def setBuildingDispatchers(path_args3, path_args2, path_args1):
 
 
 def buildNode(provider, node, source_ref, allow_none=False):
+    # too many exception handlers, pylint: disable=too-many-branches
+
     if node is None and allow_none:
         return None
 
@@ -329,6 +331,9 @@ def buildNode(provider, node, source_ref, allow_none=False):
         # User interrupting is not a problem with the source, but tell where
         # we got interrupted.
         optimization_logger.info("Interrupted at '%s'." % source_ref)
+        raise
+    except SystemExit:
+        optimization_logger.warning("Problem at '%s'." % source_ref.getAsString())
         raise
     except:
         optimization_logger.warning(
