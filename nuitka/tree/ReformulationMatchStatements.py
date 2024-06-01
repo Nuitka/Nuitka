24ce61821ecae51f909a7b8bfde0bc49f77a6140
Kay Hayen <kay.hayen@gmail.com>
2024-06-01 10:41:38 +0200

    Python3.10+: Fix, matching sequence with final star arguments could go wrong
    * This could leave the result empty depending on the matched object.

--- nuitka/tree/ReformulationMatchStatements.py
+++ nuitka/tree/ReformulationMatchStatements.py
@@ -196,8 +196,8 @@ def _buildMatchSequence(provider, pattern, make_against, source_ref):
                 assert "!" not in variable_name, variable_name
 
                 # Last one
-                if star_pos == len(pattern.patterns):
-                    slice_value = slice(count)
+                if star_pos == len(pattern.patterns) - 1:
+                    slice_value = slice(star_pos, None)
                 else:
                     slice_value = slice(count, -(len(pattern.patterns) - (count + 1)))
 
