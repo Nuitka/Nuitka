8665e69bd71fdac2504a5b9898ec4cdd14e6158b
Kay Hayen <kay.hayen@gmail.com>
2024-06-10 10:08:15 +0200

    UI: Added checks for illegal character in data file dest paths
    * It seems some people can end up using "*.something" when
      trying to copy files, that will crash on Windows. Also
      trailing space and dots are not allowed there.
    
    * Also it's a good idea to catch "\0" on all platforms.
    
    * On macOS we outlaw the ":" character, which might not be
      good.
    
    * We probably should warn about some path elements, that are
      problematic, e.g. "*.something" is still not good on
      non-Windows, but should result in warnings.

--- nuitka/utils/FileOperations.py
+++ nuitka/utils/FileOperations.py
@@ -1486,6 +1486,30 @@ def openPickleFile(filename, mode, protocol=-1):
         )
 
 
+def isLegalPath(path):
+    illegal_suffixes = "/\\"
+    illegal_chars = "\0"
+
+    if isWin32Windows():
+        illegal_chars += r'*"/<>:|?'
+
+        illegal_chars += "".join(chr(x) for x in range(1, 32))
+        illegal_suffixes += " ."
+
+    if isMacOS():
+        illegal_chars += ":"
+
+    for c in path:
+        if c in illegal_chars:
+            return False, "contains illegal character %r" % c
+
+    for illegal_suffix in illegal_suffixes:
+        if path.endswith(illegal_suffix):
+            return False, "contains illegal suffix %r" % illegal_suffix
+
+    return True, None
+
+
 #     Part of "Nuitka", an optimizing Python compiler that is compatible and
 #     integrates with CPython, but also works on its own.
 #
