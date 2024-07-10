2b38d009907ffaaf2430282c11270db3f9602b32
Kay Hayen <kay.hayen@gmail.com>
2024-07-10 13:30:18 +0200

    Watch: Allow auto-stage to accept optimization infos automatically

--- nuitka/tools/watch/AutoStage.py
+++ nuitka/tools/watch/AutoStage.py
@@ -7,6 +7,8 @@ This aims at recognizing unimportant changes automatically and is used
 for larger report format migrations in the future potentially.
 """
 
+from optparse import OptionParser
+
 from nuitka.tools.quality.Git import (
     getCheckoutFileChangeDesc,
     getFileHashContent,
@@ -16,15 +18,57 @@ from nuitka.tools.quality.Git import (
 from nuitka.TreeXML import fromString, toString
 from nuitka.utils.FileOperations import getFileContents, withTemporaryFile
 
+options = None
+
+
+def findMatchingNode(root, search_node):
+    """Find a node matching the given one in a XML root."""
+    nodes = []
+
+    node = search_node
+    while node is not None:
+        nodes.insert(0, node)
+
+        node = node.getparent()
+
+    # Root is easy and hard coded.
+    current = root
+    del nodes[0]
+
+    for node in nodes:
+        node_tag = node.tag
+
+        if node_tag == "module":
+            attrib_name = "name"
+        elif node_tag == "optimization-time":
+            attrib_name = "pass"
+        else:
+            assert False, (node, current)
+
+        attrib_value = node.attrib[attrib_name]
+
+        for candidate in current.findall(node_tag):
+            try:
+                candidate_value = candidate.attrib[attrib_name]
+            except KeyError:
+                assert False, current
+
+            if candidate_value == attrib_value:
+                current = candidate
+                break
+        else:
+            return None
+
+    return current
+
 
 def onCompilationReportChange(filename, git_stage):
-    new_report = fromString(getFileContents(filename))
+    print("Working on", filename)
+
+    new_report = fromString(getFileContents(filename, mode="rb"), use_lxml=True)
     old_git_contents = getFileHashContent(git_stage["src_hash"])
 
-    if str is not bytes:
-        old_git_contents = old_git_contents.decode("utf8")
-
-    old_report = fromString(old_git_contents)
+    old_report = fromString(old_git_contents, use_lxml=True)
 
     new_nuitka_version = new_report.attrib["nuitka_version"]
     changed = False
@@ -32,6 +76,13 @@ def onCompilationReportChange(filename, git_stage):
         old_report.attrib["nuitka_version"] = new_nuitka_version
 
         changed = True
+
+    if options.accept_optimization_time:
+        for new_node in new_report.xpath("//module/optimization-time"):
+            old_node = findMatchingNode(old_report, new_node)
+
+            if old_node is not None:
+                old_node.getparent().replace(old_node, new_node)
 
     if changed:
         new_git_contents = toString(old_report)
@@ -54,6 +105,22 @@ def onFileChange(git_stage):
 
 
 def main():
+    # Cheating in this singleton and not passing options,
+    # pylint: disable=global-statement
+    global options
+
+    parser = OptionParser()
+
+    parser.add_option(
+        "--accept-optimization-time",
+        action="store_true",
+        dest="accept_optimization_time",
+        default=False,
+        help="""Accept module optimization-time changes.""",
+    )
+
+    options, _positional_args = parser.parse_args()
+
     for git_stage in getCheckoutFileChangeDesc(staged=False):
         onFileChange(git_stage=git_stage)
 
