b0f9c293a839282fc4c341c729396e402b83bdb2
Kay Hayen <kay.hayen@gmail.com>
2024-08-15 14:43:31 +0200

    Cleanup, put NameError exceptions directly into exception state
    * No need to first put them in the thread state just to fetch back from there
    
    * Performance is strangely identical, but it's a lot of code remove that was duplicated.
    
    * Also removing the chaining that must be wrong during their creation.

--- nuitka/build/static_src/HelpersExceptions.c
+++ nuitka/build/static_src/HelpersExceptions.c
@@ -282,37 +282,6 @@ error:
 
 #endif
 
-// Raise NameError for a given variable name.
-void SET_CURRENT_EXCEPTION_NAME_ERROR(PyThreadState *tstate, PyObject *variable_name) {
-#if PYTHON_VERSION >= 0x300
-    PyObject *exception_value_str = Nuitka_String_FromFormat("name '%U' is not defined", variable_name);
-#else
-    PyObject *exception_value_str =
-        Nuitka_String_FromFormat("name '%s' is not defined", Nuitka_String_AsString_Unchecked(variable_name));
-#endif
-    PyObject *exception_value = MAKE_EXCEPTION_FROM_TYPE_ARG0(tstate, PyExc_NameError, exception_value_str);
-    Py_DECREF(exception_value_str);
-
-#if PYTHON_VERSION >= 0x300
-    CHAIN_EXCEPTION(tstate, exception_value);
-#endif
-
-    SET_CURRENT_EXCEPTION_TYPE0_VALUE1(tstate, PyExc_NameError, exception_value);
-}
-
-// Raise NameError with "global" for a given variable name.
-#if PYTHON_VERSION < 0x300
-void SET_CURRENT_EXCEPTION_GLOBAL_NAME_ERROR(PyThreadState *tstate, PyObject *variable_name) {
-    PyObject *exception_value_str =
-        Nuitka_String_FromFormat("global name '%s' is not defined", Nuitka_String_AsString_Unchecked(variable_name));
-
-    PyObject *exception_value = MAKE_EXCEPTION_FROM_TYPE_ARG0(tstate, PyExc_NameError, exception_value_str);
-    Py_DECREF(exception_value_str);
-
-    SET_CURRENT_EXCEPTION_TYPE0_VALUE1(tstate, PyExc_NameError, exception_value);
-}
-#endif
-
 //     Part of "Nuitka", an optimizing Python compiler that is compatible and
 //     integrates with CPython, but also works on its own.
 //
