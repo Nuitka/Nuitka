161ace1d2dc151a95c2832a75ab79e0c4d05c086
Kay Hayen <kay.hayen@gmail.com>
2024-12-12 15:32:40 +0100

    Test: Cleanup, use more common code for package tests
    * Scanning for test cases and main files in their should
      use common code.
    
    * Also remove commercial only test that embeds data
      files in a package.

--- tests/packages/sub_package/kitty/__init__.py
+++ tests/packages/sub_package/kitty/__init__.py
@@ -1,6 +1,8 @@
 #     Copyright 2024, Kay Hayen, mailto:kay.hayen@gmail.com find license text at end of file
 
 
+# nuitka-project: --include-package=kitty
+
 from __future__ import print_function
 
 import sys
