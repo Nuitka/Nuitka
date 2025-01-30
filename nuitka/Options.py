d0fe8647f60b3d744bbccc7c3a8c766ef4d2b336
Kay Hayen <kay.hayen@gmail.com>
2025-01-30 09:00:43 +0100

    Minor cleanup

--- nuitka/Options.py
+++ nuitka/Options.py
@@ -2739,7 +2739,7 @@ def getModuleParameter(module_name, parameter_name):
 
 
 def getForcedRuntimeEnvironmentVariableValues():
-    """:returns: iterable (string, string) derived from ``----force-runtime-environment-variable``"""
+    """:returns: iterable (string, string) derived from ``--force-runtime-environment-variable``"""
 
     for forced_runtime_env_variables_spec in options.forced_runtime_env_variables:
         name, value = forced_runtime_env_variables_spec.split("=", 1)
