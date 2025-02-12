843dc76cdc3a6e7f4e817471e147bd70a2187d36
Kay Hayen <kay.hayen@gmail.com>
2025-02-12 12:38:58 +0100

    AIX: Avoid using "$" in generated code identifiers
    * Cleanup different code used for variable and other names used.
    
    * Cannot use "$" on some platforms, one of which is gcc on AIX, since it's not supported by their assembler.

--- nuitka/ModuleRegistry.py
+++ nuitka/ModuleRegistry.py
@@ -17,6 +17,7 @@ import os
 from nuitka.containers.Namedtuples import makeNamedtupleClass
 from nuitka.containers.OrderedSets import OrderedSet
 from nuitka.PythonVersions import python_version
+from nuitka.utils.CStrings import decodePythonIdentifierFromC
 
 # One or more root modules, i.e. entry points that must be there.
 root_modules = OrderedSet()
@@ -191,15 +192,19 @@ def getModuleInclusionInfoByName(module_name):
 
 
 def getModuleFromCodeName(code_name):
+    module_name = decodePythonIdentifierFromC(code_name)
+
     # TODO: We need something to just load modules.
     for module in root_modules:
-        if module.getCodeName() == code_name:
+        if module.getCodeName() == module_name:
             return module
 
     assert False, code_name
 
 
 def getOwnerFromCodeName(code_name):
+    code_name = decodePythonIdentifierFromC(code_name)
+
     if "$$$" in code_name:
         module_code_name, _function_code_name = code_name.split("$$$", 1)
 
