6da26c17f0247b4f00cccaba45e0d0edc937ad5d
Kay Hayen <kay.hayen@gmail.com>
2025-07-19 15:49:09 +0200

    fixup! UI: Added support for using "rich" progress bar for downloads

--- nuitka/Progress.py
+++ nuitka/Progress.py
@@ -532,7 +532,7 @@ def withNuitkaDownloadProgressBar(*args, **kwargs):
             # spell-checker: ignore bsize, tsize
             def onProgress(self, b=1, bsize=1, tsize=None):
                 if tsize is not None:
-                    self.total = tsize
+                    self.total = tsize # false alarm, pylint: disable=attribute-defined-outside-init,I0021
                 self.update(b * bsize - self.n)
 
         tqdm_kwargs = kwargs.copy()
