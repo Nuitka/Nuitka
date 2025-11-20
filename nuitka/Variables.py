54a48a329a4a8baf3e214999a40b96b539ea921a
Kay Hayen <kay.hayen@gmail.com>
2025-11-20 10:39:44 +0000

    Optimization: Keep track of variables that can even escape
    * Temporary and locals dict variables cannot escape yet were
      using a lot of time in virtual method calls.
    
    * This avoids that time entirely making this step even faster.
    
    * We do not take the effort to dynamically clean it up, so
      variables could be added there that then don't exist, but
      it's still faster this way as those are mostly rare cases.

--- nuitka/Variables.py
+++ nuitka/Variables.py
@@ -305,6 +305,8 @@ class LocalVariable(Variable):
 
     def initVariable(self, trace_collection):
         """Initialize variable in trace collection state."""
+        trace_collection.variable_escapable.add(self)
+        trace_collection.has_unescaped_variables = True
         return trace_collection.initVariableUninitialized(self)
 
     if str is not bytes:
@@ -371,6 +373,8 @@ class ModuleVariable(Variable):
 
     def initVariable(self, trace_collection):
         """Initialize variable in trace collection state."""
+        trace_collection.variable_escapable.add(self)
+        trace_collection.has_unescaped_variables = True
         return trace_collection.initVariableModule(self)
 
     def onControlFlowEscape(self, trace_collection):
