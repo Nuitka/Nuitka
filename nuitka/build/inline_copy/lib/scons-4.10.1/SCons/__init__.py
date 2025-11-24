a58610a85eecce8906230451dd1e36649689c475
Kay Hayen <kay.hayen@gmail.com>
2025-11-24 13:29:32 +0000

    Scons: Added newer inline copy with Scons that supports Visual Studio 2026
    * Migrated and expanded our patches to avoid unused tools, deleted
      more than before.
    
    * Remove annotations which is what we do now for inline copies, since
      they only waste space.

---
+++ nuitka/build/inline_copy/lib/scons-4.10.1/SCons/__init__.py
@@ -0,0 +1,9 @@
+__version__="4.10.1"
+__copyright__="Copyright (c) 2001 - 2025 The SCons Foundation"
+__developer__="bdbaddog"
+__date__="Sun, 16 Nov 2025 14:18:20 -0700"
+__buildsys__="M1Dog2021"
+__revision__="055b01f429d58b686701a56df863a817c36bb103"
+__build__="055b01f429d58b686701a56df863a817c36bb103"
+# make sure compatibility is always in place
+import SCons.compat  # noqa
