c9538587700776d6e65163901dbd5b65193348ea
Peter Bierma <zintensitydev@gmail.com>
2025-11-27 13:09:44 +0000

    Python3.14: Follow change in order of evaluation of "__exit__" and "__enter__"

--- nuitka/tree/ReformulationWithStatements.py
+++ nuitka/tree/ReformulationWithStatements.py
@@ -222,7 +222,8 @@ def _buildWithNode(provider, context_expr, assign_target, body, sync, source_ref
             attribute_exit_assignment,
             enter_await_statement,
         )
-    elif python_version >= 0x360 and sync:
+    # It's weird, but 3.14 looks up __exit__ before __enter__
+    elif 0x360 <= python_version < 0x3E0 and sync:
         attribute_assignments = (attribute_enter_assignment, attribute_exit_assignment)
     else:
         attribute_assignments = (attribute_exit_assignment, attribute_enter_assignment)
