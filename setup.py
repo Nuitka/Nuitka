5f4927621fab588b9d464ddf2ab21317fe8b82ab
Kay Hayen <kay.hayen@gmail.com>
2026-01-12 10:22:46 +0100

    Python3.14: Pronounce it as supported in the docs

--- setup.py
+++ setup.py
@@ -406,6 +406,7 @@ setup(
         # Nuitka standalone mode aims at distribution
         "Topic :: System :: Software Distribution",
         # Python3 supported versions.
+        "Programming Language :: Python :: 3.14",
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.11",
