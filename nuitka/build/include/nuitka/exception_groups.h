db8f9d353fefc384e1d8052f0eef600825802323
Peter Bierma <zintensitydev@gmail.com>
2026-02-02 11:29:55 -0500

    Fix stupid formatting.

--- nuitka/build/include/nuitka/exception_groups.h
+++ nuitka/build/include/nuitka/exception_groups.h
@@ -94,8 +94,10 @@ NUITKA_MAY_BE_UNUSED static inline int EXCEPTION_GROUP_MATCH_BOOL(PyThreadState 
                                                                   PyObject *match_type, PyObject **match,
                                                                   PyObject **rest) {
     if (Py_IsNone(exc_value)) {
-        Py_INCREF_IMMORTAL(Py_None) *match = Py_None;
-        Py_INCREF_IMMORTAL(Py_None) *rest = Py_None;
+        Py_INCREF_IMMORTAL(Py_None);
+        *match = Py_None;
+        Py_INCREF_IMMORTAL(Py_None);
+        *rest = Py_None;
         return 0;
     }
     assert(PyExceptionInstance_Check(exc_value));
@@ -128,7 +130,8 @@ NUITKA_MAY_BE_UNUSED static inline int EXCEPTION_GROUP_MATCH_BOOL(PyThreadState 
             }*/
             *match = wrapped;
         }
-        Py_INCREF_IMMORTAL(Py_None) *rest = Py_None;
+        Py_INCREF_IMMORTAL(Py_None);
+        *rest = Py_None;
         return 0;
     }
 
@@ -164,7 +167,8 @@ NUITKA_MAY_BE_UNUSED static inline int EXCEPTION_GROUP_MATCH_BOOL(PyThreadState 
         return 0;
     }
     /* no match */
-    Py_INCREF_IMMORTAL(Py_None) *match = Py_None;
+    Py_INCREF_IMMORTAL(Py_None);
+    *match = Py_None;
     *rest = Py_NewRef(exc_value);
     return 0;
 }
