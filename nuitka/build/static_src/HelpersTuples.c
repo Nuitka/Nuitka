8677e32e57da9475cdd5102bf209f0357cea3bff
Kay Hayen <kay.hayen@gmail.com>
2026-04-28 15:32:13 +0200

    Python3.14: Fix, the tuple reuse was not fully compatible
    * This could potentially cause crashes due to outdated hash caches.

--- nuitka/build/static_src/HelpersTuples.c
+++ nuitka/build/static_src/HelpersTuples.c
@@ -20,6 +20,7 @@ PyObject *MAKE_TUPLE_EMPTY(PyThreadState *tstate, Py_ssize_t size) {
 
 #if PYTHON_VERSION >= 0x3e0
     Py_ssize_t index = size - 1;
+    bool needs_recycle = false;
 
     if (index < PyTuple_MAXSAVESIZE) {
         result_tuple = (PyTupleObject *)Nuitka_PyFreeList_Pop(&_Py_freelists_GET()->tuples[index]);
@@ -30,8 +31,8 @@ PyObject *MAKE_TUPLE_EMPTY(PyThreadState *tstate, Py_ssize_t size) {
     if (result_tuple == NULL) {
         result_tuple = (PyTupleObject *)Nuitka_GC_NewVar(&PyTuple_Type, size);
     } else {
-        _PyTuple_RESET_HASH_CACHE(result_tuple);
         Nuitka_Py_NewReference((PyObject *)result_tuple);
+        needs_recycle = true;
     }
 #else
     // This is the CPython name, spell-checker: ignore numfree
@@ -79,7 +80,18 @@ PyObject *MAKE_TUPLE_EMPTY(PyThreadState *tstate, Py_ssize_t size) {
         result_tuple->ob_item[i] = NULL;
     }
 
+#if PYTHON_VERSION >= 0x3e0
+    // Python 3.14 tuples carry a cached hash value and have a dedicated
+    // recycler for freelist entries.
+    if (needs_recycle) {
+        _PyTuple_Recycle((PyObject *)result_tuple);
+    } else {
+        _PyTuple_RESET_HASH_CACHE((PyObject *)result_tuple);
+        Nuitka_GC_Track(result_tuple);
+    }
+#else
     Nuitka_GC_Track(result_tuple);
+#endif
 
     assert(PyTuple_CheckExact(result_tuple));
     assert(PyTuple_GET_SIZE(result_tuple) == size);
