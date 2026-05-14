4f0292d4c18794218833dc2c7cc007e2b12c208c
Peter Bierma <zintensitydev@gmail.com>
2026-05-14 05:21:44 -0400

    Python3.14: Implement `__annotate__` for classes
    * Also delay class level annotations if the experimental flag is given
    
    ---------
    
    Co-authored-by: Kay Hayen <kay.hayen@gmail.com>

--- nuitka/nodes/ClassNodes.py
+++ nuitka/nodes/ClassNodes.py
@@ -7,6 +7,7 @@ The classes are are at the core of the language and have their complexities.
 
 """
 
+from nuitka.containers.OrderedDicts import OrderedDict
 from nuitka.containers.OrderedSets import OrderedSet
 from nuitka.PythonVersions import python_version
 
@@ -124,7 +125,12 @@ class ExpressionClassMappingBody(MarkNeedsAnnotationsMixin, ExpressionClassBodyB
 
     kind = "EXPRESSION_CLASS_MAPPING_BODY"
 
-    __slots__ = ("needs_annotations_dict", "qualname_setup", "static_attributes")
+    __slots__ = (
+        "needs_annotations_dict",
+        "qualname_setup",
+        "static_attributes",
+        "deferred_annotations",
+    )
 
     # Force creation with proper type.
     locals_kind = "python_mapping_class"
@@ -142,6 +148,7 @@ class ExpressionClassMappingBody(MarkNeedsAnnotationsMixin, ExpressionClassBodyB
 
         self.qualname_setup = None
         self.static_attributes = OrderedSet() if python_version >= 0x3D0 else None
+        self.deferred_annotations = OrderedDict() if python_version >= 0x3E0 else None
 
     def addStaticAttribute(self, static_attribute):
         self.static_attributes.add(static_attribute)
