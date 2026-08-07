7b42916eef48f121573ca67c9207a691716000ac
Kay Hayen <kay.hayen@gmail.com>
2026-08-07 13:32:41 +0200

    Python3.15: Need to declare API for it to remain usable for us

--- nuitka/build/static_src/MainProgram.c
+++ nuitka/build/static_src/MainProgram.c
@@ -1478,6 +1478,9 @@ extern char const *getBinaryFilenameHostEncoded(bool resolve_symlinks);
 #if PYTHON_VERSION >= 0x3d0
 PyAPI_FUNC(void) PySys_AddWarnOption(const wchar_t *s);
 #endif
+#if PYTHON_VERSION >= 0x3f0
+PyAPI_FUNC(void) PySys_ResetWarnOptions(void);
+#endif
 
 // Preserve and provide the original argv[0] as recorded by the bootstrap stage.
 static native_command_line_argument_t const *original_argv0 = NULL;
