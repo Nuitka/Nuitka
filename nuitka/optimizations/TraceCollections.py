835246e96a71a90820502c8698398ff4e3750bf5
Kay Hayen <kay.hayen@gmail.com>
2026-08-11 20:57:17 +0200

    Fix, optimization was not fully determistic
    * We iterate over the set of these variables and order changes
      can cause behavior differences

--- nuitka/optimizations/TraceCollections.py
+++ nuitka/optimizations/TraceCollections.py
@@ -326,7 +326,7 @@ class TraceCollectionBase(object):
         # Even though it's empty, we set it, because init of variables won't do it.
         self.has_unescaped_variables = True
 
-        self.variable_escapable = set()
+        self.variable_escapable = OrderedSet()
 
     def __repr__(self):
         return "<%s for %s at 0x%x>" % (self.__class__.__name__, self.name, id(self))
