764d42a2b73e38b8e1fe0fac48238d882b9ba0bf
Kay Hayen <kay.hayen@gmail.com>
2026-08-12 11:53:17 +0200

    Watch: More corrects for automatic staging of changes

--- nuitka/tools/quality/Git.py
+++ nuitka/tools/quality/Git.py
@@ -11,6 +11,7 @@ spell-checker: ignore Hallett,unpushed
 
 import os
 import re
+import subprocess
 
 from nuitka.containers.OrderedSets import OrderedSet
 from nuitka.format.FileFormatting import cleanupWindowsNewlines
@@ -24,6 +25,7 @@ from nuitka.utils.Execution import (
     executeProcess,
 )
 from nuitka.utils.FileOperations import openTextFile
+from nuitka.utils.Utils import decoratorRetries
 
 
 # Parse output from `git diff-index`
@@ -185,6 +187,14 @@ def putFileHashContent(filename):
     return new_hash.rstrip()
 
 
+@decoratorRetries(
+    logger=tools_logger,
+    purpose="update git index",
+    consequence="Autostaging of compilation report may not take effect.",
+    attempts=3,
+    exception_type=(OSError, subprocess.CalledProcessError),
+    sleep_time=1,
+)
 def updateFileIndex(diff_entry, new_object_hash):
     """Update the git index with a new hash for a file."""
     # spell-checker: ignore cacheinfo
