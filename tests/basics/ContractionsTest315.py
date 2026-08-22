872b5ac6d3924913fd4a1aee18caf0295e8e6354
Kay Hayen <kay.hayen@gmail.com>
2026-08-22 18:02:46 +0200

    fixup! Python3.15: Added support for unpacking in comprehensions

--- tests/basics/ContractionsTest315.py
+++ tests/basics/ContractionsTest315.py
@@ -1,3 +1,6 @@
+#     Copyright 2026, Kay Hayen, mailto:kay.hayen@gmail.com find license text at end of file
+
+
 print([*i for i in [[1, 2, 3], [4, 5, 6]]])
 print({*i for i in [[1, 2, 3], [4, 5, 6]]})
 print(list((*i for i in [[1, 2, 3], [4, 5, 6]])))
@@ -42,3 +45,18 @@ def run_coro(coro):
 run_coro(listComp())
 run_coro(setComp())
 run_coro(genExp())
+
+#     Python tests originally created or extracted from other peoples work. The
+#     parts were too small to be protected.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
